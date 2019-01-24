d550559f58b078848d8eb0e611247fdcb8d62cdc
Marc <medwards219@gmail.com>
2019-01-24 09:02:00 -0800

    .bat edits

--- conversion/load_ab_photoYear.bat
+++ conversion/load_ab_photoYear.bat
@@ -2,10 +2,13 @@
 
 ::The format of the source dataset is a single shapefile
 
-::Load into a target table in a schema named 'ab'. Schema should be created manually in PostgreSQL before loading.
+::Load into a target table in a schema named 'ab'.
 
 ::Multipart and single part polygons are mixed in same file. This can be seen in arcgis by adding a count field and running !Shape!.partCount in the python field calculator. 
 ::Solution is to set the -nlt argument to PROMOTE_TO_MULTI. This auto converts all features to multipart features when loading.
+
+::Create schema if it doesn't exist
+ogrinfo PG:"host=localhost dbname=cas user=postgres password=postgres" -sql "CREATE SCHEMA IF NOT EXISTS ab";
 
 ::Source file location
 SET srcF=C:\Temp\AB_Photo_Year\PhotoYear_Update.shp
@@ -13,8 +16,7 @@ SET srcF=C:\Temp\AB_Photo_Year\PhotoYear_Update.shp
 ::Target table name
 SET trgtT=ab.AB_PhotoYear
 
-::Target projection - Canada Albers Equal Area Conic
-:: %~dp0 fetches the directory that contains the batch file. Use this to point to the prj file stored with the scripts
+::Projection file. Canada Albers Equal Area Conic. %~dp0 fetches the directory that contains the batch file.
 SET batchDir=%~dp0
 SET prjF="%batchDir%canadaAlbersEqualAreaConic.prj"
 
