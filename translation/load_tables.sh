8d758b807b4e248baec5353daf373b9b2acee5c3
Marc <medwards219@gmail.com>
2019-01-29 10:35:25 -0800

    Translation table loading script in root, csv's moved to tables folder

--- translation/load_tables.sh
+++ translation/load_tables.sh
@@ -4,33 +4,38 @@
 
 # Input tables are format csv
 
-# Provide either a manual list of files:
-declare -a file_list=("ab_0006/rules_lyr.csv" "bc_0008/species.csv")
+# Two options for loading tables:
+	# 1. User can provide an array of csv table paths to be loaded. 
+	# 2. Or user can provide the file path to a folder in which case all csv files in the folder are loaded 
+	# One or both of these options can be provided.
+# If table already exists, loading will fail and move on to the next table. To overwrite existing tables, un-comment the -overwrite line in the ogr2ogr call.
 
-# or, a folder containing csv's to be loaded:
-#load_folder="ab_0016/"
+#####################################################################################################################################################################
+#####################################################################################################################################################################
+#####################################################################################################################################################################
+# Provide either...
+
+#### 1. #### a manual list of files:
+#declare -a file_list=("tables/ab_0006_rules_lyr.csv" "tables/nb_0001_species.csv")
+
+# And/Or
+
+#### 2. #### a folder containing csv's to be loaded:
+load_folder="tables/"
 
 # target schema name
-target_schema=test
+target_schema=translation_tables
+
+# gdal folder path
+gdal_path="../../../../../../../program files/gdal"
+#####################################################################################################################################################################
+#####################################################################################################################################################################
+#####################################################################################################################################################################
 
 # make schema if it doesn't exist
-"../../../../../../../program files/gdal/ogrinfo.exe" PG:"host=localhost dbname=cas user=postgres password=postgres" -sql "CREATE SCHEMA IF NOT EXISTS test";
+"$gdal_path/ogrinfo.exe" PG:"host=localhost dbname=cas user=postgres password=postgres" -sql "CREATE SCHEMA IF NOT EXISTS $target_schema";
 
-# if the load_folder variable exists, load all files in the folder
-if [ -n "$load_folder" ]; then 
-	for i in $load_folder*
-	do
-		x=${i##*/} # gets file name with .csv
-		tab_name=${x%%.csv} # removes .csv
-		schema_tab=$target_schema.$tab_name # combines table name and schema name
-		
-		# load using ogr
-		"../../../../../../../program files/gdal/ogr2ogr.exe" \
-		-f "PostgreSQL" "PG:host=localhost dbname=cas user=postgres password=postgres" $i \
-		-nln $schema_tab
-	done
-fi
-
+#### 1. ####
 # if the file_list variable exists, load the files in the manual list
 if [ -n "$file_list" ]; then 
 	for i in "${file_list[@]}"
@@ -40,8 +45,26 @@ if [ -n "$file_list" ]; then
 		schema_tab=$target_schema.$tab_name # combines table name and schema name
 		
 		# load using ogr
-		"../../../../../../../program files/gdal/ogr2ogr.exe" \
+		echo "loading..."$schema_tab
+		"$gdal_path/ogr2ogr.exe" \
 		-f "PostgreSQL" "PG:host=localhost dbname=cas user=postgres password=postgres" $i \
-		-nln $schema_tab
+		-nln $schema_tab #-overwrite
+	done
+fi
+
+#### 2. ####
+# if the load_folder variable exists, load all files in the folder
+if [ -n "$load_folder" ]; then 
+	for i in $load_folder/*.csv
+	do
+		x=${i##*/} # gets file name with .csv
+		tab_name=${x%%.csv} # removes .csv
+		schema_tab=$target_schema.$tab_name # combines table name and schema name
+		
+		# load using ogr
+		echo "loading..."$schema_tab
+		"$gdal_path/ogr2ogr.exe" \
+		-f "PostgreSQL" "PG:host=localhost dbname=cas user=postgres password=postgres" $i \
+		-nln $schema_tab #-overwrite
 	done
 fi
