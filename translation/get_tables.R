9073a25ea77013baf70419888721adbbc8766b9e
Pierre Vernier <pierre.vernier@gmail.com>
2019-02-08 14:34:47 -0800

    Update get_tables.R

--- translation/get_tables.R
+++ translation/get_tables.R
@@ -1,6 +1,8 @@
-# Download worksheets from CASFRI workbooks
+# Download translation worksheets from CASFRI workbooks
+# PV 2019-02-08
+
 library(googlesheets)
-suppressMessages(library(dplyr))
+library(dplyr)
 
 # Download rules from google docs
 bc_gs = c("ab_0006","ab_0016","bc_0008","nb_0001")
@@ -9,21 +11,4 @@ for (bc in bc_gs) {
     for (ws in gs_ws_ls(gs)) {
         gs_title(bc) %>% gs_download(ws=ws, to=paste0("tables/",substr(bc,1,7),"_rules_",ws,".csv"), overwrite=TRUE)
     }    
-}
-
-
-inventories = c("ab_0006","ab_0016","bc_0008","nb_0001")
-tables = c("cas","dst","eco","hdr","lyr","nfl","species")
-gitdir = "../../casfri/translation/tables/"
-
-for (i in inventories) {
-    for (j in tables) {
-        if (j=="species") {
-            print(paste0(gitdir,i,"_",j,".csv"))
-            #file.copy(paste0(i,"/",j), paste0(gitdir,i,"_",j,".csv"))
-        } else {
-            print(paste0(gitdir,i,"_rules_",j,".csv"))
-            #file.copy(paste0(i,"/",j), paste0(gitdir,i,"_rules_",j,".csv"))        
-        }
-    }
 }
