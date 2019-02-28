a88a0051ab04e3fa4f9ae886618396f2e782c811
Marc <medwards219@gmail.com>
2019-02-27 16:24:50 -0800

    Applied #31 to photo year script

--- conversion/bat/load_ab_photoYear.bat
+++ conversion/bat/load_ab_photoYear.bat
@@ -1,56 +1,51 @@
-::This script loads the Alberta photo year file into PostgreSQL
+:: This script loads the Alberta photo year file into PostgreSQL
 
-::The format of the source dataset is a single shapefile
+:: The format of the source dataset is a single shapefile
 
-::Load into a target table in a schema named 'ab'.
+:: Load into a target table in the schema defined in the config file.
 
-::If table already exists it will be overwritten.
+:: If the table already exists, it can be overwritten by setting the "overwriteFRI" variable 
+:: in the configuration file.
 
-::Multipart and single part polygons are mixed in same file. This can be seen in arcgis by adding a count field and running !Shape!.partCount in the python field calculator. 
-::Solution is to set the -nlt argument to PROMOTE_TO_MULTI. This auto converts all features to multipart features when loading.
+:: Multipart and single part polygons are mixed in same file. This can be seen in arcgis by adding a count field and running !Shape!.partCount in the python field calculator. 
+:: Solution is to set the -nlt argument to PROMOTE_TO_MULTI. This auto converts all features to multipart features when loading.
 
-::######################################## variables #######################################
+::######################################## Set variables #######################################
+
+SETLOCAL
 
 :: load config variables
-if exist "%~dp0\config.bat" ( 
-  call "%~dp0\config.bat"
+if exist "%~dp0\..\..\config.bat" ( 
+  call "%~dp0\..\..\config.bat"
 ) else (
   echo ERROR: NO config.bat FILE
   exit /b
 )
 
-:: PostgreSQL variables
-SET schema=test
-SET trgtT=ab_photoyear
+SET srcFileName=PhotoYear_Update
+SET srcFullPath="%friDir%/AB/PhotoYear/%srcFileName%.shp"
 
-SET srcF="%friDir%\PhotoYear_Update.shp"
+SET prjFile="%~dp0\canadaAlbersEqualAreaConic.prj"
+SET fullTargetTableName=%targetFRISchema%.ab_photoYear
 
-::##########################################################################################
 
+if %overwriteFRI% == True (
+  SET overwrite_tab=-overwrite 
+) else (
+  SET overwrite_tab=
+)
 
 ::############################ Script - shouldn't need editing #############################
 
-::Set schema.table
-SET schTab=%schema%.%trgtT%
-
-::Create schema if it doesn't exist
-ogrinfo PG:"host=%pghost% dbname=%pgdbname% user=%pguser% password=%pgpassword%" -sql "CREATE SCHEMA IF NOT EXISTS %schema%";
+:: Create schema if it doesn't exist
+"%gdalFolder%/ogrinfo" PG:"host=%pghost% dbname=%pgdbname% user=%pguser% password=%pgpassword%" -sql "CREATE SCHEMA IF NOT EXISTS %targetFRISchema%";
 
 ::Run ogr2ogr
-ogr2ogr ^
--f "PostgreSQL" PG:"host=%pghost% dbname=%pgdbname% user=%pguser% password=%pgpassword%" %srcF% ^
--nln %schTab% ^
--t_srs %prjF% ^
+"%gdalFolder%/ogr2ogr" ^
+-f "PostgreSQL" PG:"host=%pghost% dbname=%pgdbname% user=%pguser% password=%pgpassword%" %srcFullPath% ^
+-nln %fullTargetTableName% ^
+-t_srs %prjFile% ^
 -nlt PROMOTE_TO_MULTI ^
--progress -overwrite
+-progress %overwrite_tab%
 
-::unload variables
-SET schema=
-SET trgtT=
-SET srcF=
-SET pghost=
-SET pgdbname=
-SET pguser=
-SET pgpassword=
-SET friDir=
-SET prjF=
+ENDLOCAL
