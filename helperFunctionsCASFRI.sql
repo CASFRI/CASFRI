ad1d778cb4f692008ca7b4b8e2e3de92ee260ddb
Marc <medwards219@gmail.com>
2019-03-06 16:23:17 -0800

    Finished TT_CasId() and added test script

--- helperFunctionsCASFRI.sql
+++ helperFunctionsCASFRI.sql
@@ -20,12 +20,12 @@
 -------------------------------------------------------------------------------
 
 -------------------------------------------------------------------------------
--- casId1 - used for ab06, nb01
+-- casId
 --
 -- inventoryId text - table name (e.g. 'BC08')
 -- sourceFile - original name of source file, stored in src_filename create during loading
 -- mapsheetId - mapsheet name
--- objectIdCol - object id from source data
+-- polygonIdCol - polygon id from source data
 -- rowIdCol - unique row id, e.g. ogc_fid primary key created on loading
 --
 -- Arguments can be supplied as:
@@ -36,22 +36,29 @@
 --
 -- Examples
 -- AB06
--- SELECT casId1('ab06', src_filename::text, trm_1::text, poly_num::text, ogc_fid::text) AS CAS_ID FROM rawfri.ab06;
+-- SELECT TT_casId('ab06', src_filename::text, trm_1::text, poly_num::text, ogc_fid::text) AS CAS_ID FROM rawfri.ab06;
+-- In lookup table would be written: casId('ab06', src_filename, trm_1, poly_num, ogc_fid)
 --
 -- NB01 - only FOREST.shp had the STDLAB attribute so objectId for many attributes will be 0
--- SELECT casId1('nb01', src_filename::text, '', stdlab::text, ogc_fid::text) AS CAS_ID FROM rawfri.nb01;
+-- SELECT TT_casId('nb01', src_filename::text, '', stdlab::text, ogc_fid::text) AS CAS_ID FROM rawfri.nb01;
+-- In lookup table would be written: casId('nb01', src_filename, '', stdlab, ogc_fid)
 --
 -- AB16 - We can use src_filename as the mapsheet instead of reconstructing the mapsheet name using the township, range, and meridian attributes.
 --        - Letters will be lower case, cas04 was upper. Could change by making uppercase during loading.        
 --      - FilenameCol will be a text string 'CANFOR' in this case
--- SELECT casId1('ab16', 'CANFOR', src_filename::text, forest_id_2::text, ogc_fid::text) AS CAS_ID FROM rawfri.ab16;
+-- SELECT TT_casId('ab16', 'CANFOR', src_filename::text, forest_id_2::text, ogc_fid::text) AS CAS_ID FROM rawfri.ab16;
+-- In lookup table would be written: casId('nb01', src_filename, '', stdlab, ogc_fid)
+-- 
+-- BC08 - cas04 has SOURCE_ID column, don't know where this came from?
+-- SELECT TT_casId('bc08', 'VEG_COMP_LYR_R1', map_id::text, feature_id::text, ogc_fid::text) AS CAS_ID FROM rawfri.bc08;
+-- In lookup table would be written: casId('bc08', 'VEG_COMP_LYR_R1', map_id, feature_id, ogc_fid)
 -------------------------------------------------------------------------------
-DROP FUNCTION IF EXISTS casId1(text,text,text,text,text);
-CREATE OR REPLACE FUNCTION casId1(
+-- DROP FUNCTION IF EXISTS TT_casId(text,text,text,text,text);
+CREATE OR REPLACE FUNCTION TT_casId(
   inventoryId text,
   sourceFile text,
   mapsheetId text,
-  objectId text,
+  polygonIdCol text,
   rowId text
 )
 RETURNS text AS $$
@@ -62,10 +69,10 @@ RETURNS text AS $$
     var4 text;
     var5 text;
   BEGIN
-    var1 = TT_Pad(inventoryId,4,'x');
-    var2 = TT_Pad(sourceFile,15,'x');
-    var3 = TT_Pad(mapsheetId,10, 'x');
-    var4 = TT_Pad(objectId,10,'0');
+    var1 = TT_Pad(upper(inventoryId),4,'x');
+    var2 = TT_Pad(upper(sourceFile),15,'x');
+    var3 = TT_Pad(upper(mapsheetId),10, 'x');
+    var4 = TT_Pad(polygonIdCol,10,'0');
     var5 = TT_Pad(rowId,7,'0');
     RETURN TT_Concat('-', FALSE, var1, var2, var3, var4, var5);
   END;
---
+++ helperFunctionsCASFRITest.sql
@@ -0,0 +1,93 @@
+﻿------------------------------------------------------------------------------
+-- PostgreSQL Table Tranlation Engine - Test file
+-- Version 0.1 for PostgreSQL 9.x
+-- https://github.com/edwardsmarc/postTranslationEngine
+--
+-- This is free software; you can redistribute and/or modify it under
+-- the terms of the GNU General Public Licence. See the COPYING file.
+--
+-- Copyright (C) 2018-2020 Pierre Racine <pierre.racine@sbf.ulaval.ca>, 
+--                         Marc Edwards <medwards219@gmail.com>,
+--                         Pierre Vernier <pierre.vernier@gmail.com>
+--
+-------------------------------------------------------------------------------
+SET lc_messages TO 'en_US.UTF-8';
+
+-- IsError(text)
+-- function to test if helper functions return errors
+CREATE OR REPLACE FUNCTION IsError(
+  functionString text
+)
+RETURNS boolean AS $$
+  DECLARE
+    result boolean;
+  BEGIN
+    EXECUTE functionString INTO result;
+    RETURN FALSE;
+  EXCEPTION WHEN OTHERS THEN
+    RETURN TRUE;
+  END;
+$$ LANGUAGE plpgsql VOLATILE;
+
+-----------------------------------------------------------
+-- Comment out the following line and the last one of the file to display 
+-- only failing tests
+--SELECT * FROM (
+-----------------------------------------------------------
+-- The first table in the next WITH statement list all the function tested
+-- with the number of test for each. It must be adjusted for every new test.
+-- It is required to list tests which would not appear because they failed
+-- by returning nothing.
+WITH test_nb AS (
+    SELECT 'TT_CasId'::text function_tested, 1 maj_num, 4 nb_test
+     
+
+
+),
+test_series AS (
+-- Build a table of function names with a sequence of number for each function to be tested
+SELECT function_tested, maj_num, generate_series(1, nb_test)::text min_num
+FROM test_nb
+)
+SELECT coalesce(maj_num || '.' || min_num, b.number) AS number,
+       coalesce(a.function_tested, 'ERROR: Insufficient number of test for ' || 
+                b.function_tested || ' in the initial table...') AS function_tested,
+       description, 
+       NOT passed IS NULL AND (regexp_split_to_array(number, '\.'))[2] = min_num AND passed passed
+FROM test_series AS a FULL OUTER JOIN (
+
+---------------------------------------------------------
+---------------------------------------------------------
+-- Test 1 - TT_CasId
+---------------------------------------------------------
+SELECT '1.1'::text number,
+       'TT_CasId'::text function_tested,
+       'Test AB06'::text description,
+       TT_CasId('ab06', 'GB_S21_TWP', '81145', '811451038', '1') = 'AB06-xxxxxGB_S21_TWP-xxxxx81145-0811451038-0000001' passed
+---------------------------------------------------------
+UNION ALL
+SELECT '1.2'::text number,
+       'TT_CasId'::text function_tested,
+       'Test AB16'::text description,
+       TT_CasId('ab16', 'CANFOR', 't059R04M6', '109851', '1') = 'AB16-xxxxxxxxxCANFOR-xT059R04M6-0000109851-0000001' passed
+---------------------------------------------------------
+UNION ALL
+SELECT '1.3'::text number,
+       'TT_CasId'::text function_tested,
+       'Test NB01'::text description,
+       TT_CasId('nb01', 'waterbody', '', '', '1') = 'NB01-xxxxxxWATERBODY-xxxxxxxxxx-0000000000-0000001' passed
+---------------------------------------------------------
+UNION ALL
+SELECT '1.4'::text number,
+       'TT_CasId'::text function_tested,
+       'Test BC08'::text description,
+       TT_CasId('bc08', 'VEG_COMP_LYR_R1', '83D093', '2035902', '1') = 'BC08-VEG_COMP_LYR_R1-xxxx83D093-0002035902-0000001' passed
+---------------------------------------------------------
+              
+) AS b 
+ON (a.function_tested = b.function_tested AND (regexp_split_to_array(number, '\.'))[2] = min_num)
+ORDER BY maj_num::int, min_num::int
+-- This last line has to be commented out, with the line at the beginning,
+-- to display only failing tests...
+--) foo WHERE NOT passed;
+
