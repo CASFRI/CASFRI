62492be9220b10bb95b61a05c1f7b6904aad4b75
Pierre Racine <pierre.racine@sbf.ulaval.ca>
2019-03-11 14:24:58 -0400

    Closes #41

---
+++ translation/drop_tables.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE If EXISTS translation.ab06_avi01_lyr;
+DROP TABLE If EXISTS translation.ab16_avi01_lyr;
+DROP TABLE If EXISTS translation.bc08_vri01_lyr;
+DROP TABLE If EXISTS translation.nb01_nbi01_lyr;
+
+DROP TABLE If EXISTS translation.ab_avi01_crown_closure;
+DROP TABLE If EXISTS translation.ab_avi01_height;
+DROP TABLE If EXISTS translation.ab_avi01_species;
+DROP TABLE If EXISTS translation.bc_vri01_species;
+DROP TABLE If EXISTS translation.nb_nbi01_crown_closure;
+DROP TABLE If EXISTS translation.nb_nbi01_species;
