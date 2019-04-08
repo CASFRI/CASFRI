b877347424ca2c1048bb42f0189ba83390c6e5f8
Marc <medwards219@gmail.com>
2019-04-08 12:32:42 -0700

    Replacing these until engine updates are stable

---
+++ helperfunctions/helperFunctionsCasfriUninstall.sql
@@ -0,0 +1,17 @@
+﻿------------------------------------------------------------------------------
+-- CASFRI Helper functions uninstall file for CASFR v5 beta
+-- For use with PostgreSQL Table Tranlation Engine v0.1 for PostgreSQL 9.x
+-- https://github.com/edwardsmarc/postTranslationEngine
+-- https://github.com/edwardsmarc/casfri
+--
+-- This is free software; you can redistribute and/or modify it under
+-- the terms of the GNU General Public Licence. See the COPYING file.
+--
+-- Copyright (C) 2018-2020 Pierre Racine <pierre.racine@sbf.ulaval.ca>, 
+--                         Marc Edwards <medwards219@gmail.com>,
+--                         Pierre Vernier <pierre.vernier@gmail.com>
+--
+--
+--
+-------------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS TT_Casid(text,text,text,text,text);
