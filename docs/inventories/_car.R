53bf59e58de3d3f9bf8bf99193f7a19cb4b65839
Pierre R Vernier <pierre.vernier@gmail.com>
2019-06-05 17:19:58 -0700

    Completed v1 of nb01_lyr.Rmd

--- docs/inventories/_car.R
+++ docs/inventories/_car.R
@@ -19,7 +19,7 @@ friConnect = function(inv) {
     } else if (inv=="bc08") {
         x = as_tibble(dbGetQuery(con, "SELECT map_id, feature_id, inventory_standard_cd, soil_moisture_regime_1, crown_closure, proj_height_1, layer_id, species_cd_1, species_pct_1, species_cd_2, site_index, est_site_index, est_site_index_source_cd, proj_age_1, non_veg_cover_type_1, non_veg_cover_pct_1, land_cover_class_cd_1, land_cover_class_cd_2, land_cover_class_cd_3, bclcs_level_1, bclcs_level_2, bclcs_level_3, bclcs_level_4, bclcs_level_5, non_forest_descriptor, non_productive_descriptor_cd, non_productive_cd, for_mgmt_land_base_ind, line_5_vegetation_cover, line_6_site_prep_history, line_7b_disturbance_history, line_8_planting_history, reference_year, projected_date, reference_date, shape_length, shape_area, bec_zone_code, bec_subzone, bec_variant, bec_phase, site_position_meso FROM rawfri.bc08"))
     } else if (inv=="nb01") {
-        x = as_tibble(dbGetQuery(con, "SELECT ogc_fid, fst, l1cc, l1ht, l1s1, l1pr1, l1estyr, l1trt, l1trtyr, l1vs, l2cc, l2ht, l2s1, l2pr1, l2estyr, l2trt, l2trtyr, l2vs FROM rawfri.nb01"))
+        x = as_tibble(dbGetQuery(con, "SELECT ogc_fid, fst, sitei, l1cc, l1ht, l1s1, l1pr1, l1estyr, l1trt, l1trtyr, l1vs, l2cc, l2ht, l2s1, l2pr1, l2estyr, l2trt, l2trtyr, l2vs FROM rawfri.nb01"))
         #x = as_tibble(dbGetQuery(con, "SELECT * FROM rawfri.nb01"))
     } else {
         stop('There is no inventory with that name!')
