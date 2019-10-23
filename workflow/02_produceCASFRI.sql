6aecda154cc22f683f244c8202a41a4d2f482135
Pierre <pierre.racine@sbf.ulaval.ca>
2019-10-23 10:04:33 -0400

    Updated production times following logging optimization.

--- workflow/02_produceCASFRI.sql
+++ workflow/02_produceCASFRI.sql
@@ -39,17 +39,17 @@ SELECT * FROM TT_Translate_ab06_cas('rawfri', 'ab06', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab06_avi01_cas');
 ------------------------
-INSERT INTO casfri50.cas_all -- 2h11m
+INSERT INTO casfri50.cas_all -- 37m35s
 SELECT * FROM TT_Translate_ab16_cas('rawfri', 'ab16', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab16_avi01_cas');
 ------------------------
-INSERT INTO casfri50.cas_all -- 2h9m
+INSERT INTO casfri50.cas_all -- 2h45m
 SELECT * FROM TT_Translate_nb01_cas('rawfri', 'nb01', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'nb01_nbi01_cas');
 ------------------------
-INSERT INTO casfri50.cas_all -- 9h13m -- 8h37
+INSERT INTO casfri50.cas_all -- 12h16m
 SELECT * FROM TT_Translate_bc08_cas('rawfri', 'bc08', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'bc08_vri01_cas');
@@ -64,22 +64,22 @@ SELECT TT_Prepare('translation', 'nb01_nbi01_dst', '_nb01_dst', 'ab06_avi01_dst'
 SELECT TT_Prepare('translation', 'bc08_vri01_dst', '_bc08_dst', 'ab06_avi01_dst');
 ------------------------
 --DROP TABLE IF EXISTS casfri50.dst_all;
-CREATE TABLE casfri50.dst_all AS -- 2m -- 1m35s
+CREATE TABLE casfri50.dst_all AS -- 2m12s
 SELECT * FROM TT_Translate_ab06_dst('rawfri', 'ab06', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab06_avi01_dst');
 ------------------------
-INSERT INTO casfri50.dst_all -- 21m54s -- 15m51s
+INSERT INTO casfri50.dst_all -- 24m46s
 SELECT * FROM TT_Translate_ab16_dst('rawfri', 'ab16', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab16_avi01_dst');
 ------------------------
-INSERT INTO casfri50.dst_all -- 1h49m -- 1h12m
+INSERT INTO casfri50.dst_all -- 1h32m
 SELECT * FROM TT_Translate_nb01_dst('rawfri', 'nb01', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'nb01_nbi01_dst');
 ------------------------
-INSERT INTO casfri50.dst_all -- 8h43m - 5h44m
+INSERT INTO casfri50.dst_all -- 7h3m
 SELECT * FROM TT_Translate_bc08_dst('rawfri', 'bc08', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'bc08_vri01_dst');
@@ -94,22 +94,22 @@ SELECT TT_Prepare('translation', 'nb01_nbi01_eco', '_nb01_eco', 'ab06_avi01_eco'
 SELECT TT_Prepare('translation', 'bc08_vri01_eco', '_bc08_eco', 'ab06_avi01_eco');
 ------------------------
 --DROP TABLE IF EXISTS casfri50.eco_all;
-CREATE TABLE casfri50.eco_all AS -- 44s -- 29s
+CREATE TABLE casfri50.eco_all AS -- 36s
 SELECT * FROM TT_Translate_ab06_eco('rawfri', 'ab06', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab06_avi01_eco');
 ------------------------
-INSERT INTO casfri50.eco_all -- 7m13s -- 4m17s
+INSERT INTO casfri50.eco_all -- 6m2s
 SELECT * FROM TT_Translate_ab16_eco('rawfri', 'ab16', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab16_avi01_eco');
 ------------------------
-INSERT INTO casfri50.eco_all -- 51m31s
+INSERT INTO casfri50.eco_all -- 1h27m
 SELECT * FROM TT_Translate_nb01_eco('rawfri', 'nb01', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'nb01_nbi01_eco');
 ------------------------
-INSERT INTO casfri50.eco_all -- 4h37m - 3h14m
+INSERT INTO casfri50.eco_all -- 4h05m
 SELECT * FROM TT_Translate_bc08_eco('rawfri', 'bc08', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'bc08_vri01_eco');
@@ -140,27 +140,27 @@ SELECT TT_Prepare('translation', 'nb01_nbi01_lyr', '_nb01_lyr', 'ab06_avi01_lyr'
 SELECT TT_Prepare('translation', 'bc08_vri01_lyr', '_bc08_lyr', 'ab06_avi01_lyr');
 -------------------------
 --DROP TABLE IF EXISTS casfri50.lyr_all;
-CREATE TABLE casfri50.lyr_all AS -- 4m9s -- 3m42s
+CREATE TABLE casfri50.lyr_all AS -- 4m41s
 SELECT * FROM TT_Translate_ab06_lyr('rawfri', 'ab06', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab06_avi01_lyr');
 ------------------------
-INSERT INTO casfri50.lyr_all -- 45m34s -- 38m51s
+INSERT INTO casfri50.lyr_all -- 46m20s
 SELECT * FROM TT_Translate_ab16_lyr('rawfri', 'ab16', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab16_avi01_lyr');
 ------------------------
-INSERT INTO casfri50.lyr_all -- 4h50m -- 4h44m
+INSERT INTO casfri50.lyr_all -- 5h32m
 SELECT * FROM TT_Translate_nb01_lyr('rawfri', 'nb01', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'nb01_nbi01_lyr');
 ------------------------
-INSERT INTO casfri50.lyr_all -- 26h31m
+INSERT INTO casfri50.lyr_all -- 30h19m
 SELECT * FROM TT_Translate_bc08_lyr('rawfri', 'bc08', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'bc08_vri01_lyr');
 ------------------------
-SELECT count(*) FROM casfri50.lyr_all; -- xx
+SELECT count(*) FROM casfri50.lyr_all; -- 5736548
 -------------------------------------------------------
 -- Translate all NFL tables into a common table
 -------------------------------------------------------
@@ -170,22 +170,22 @@ SELECT TT_Prepare('translation', 'nb01_nbi01_nfl', '_nb01_nfl', 'ab06_avi01_nfl'
 SELECT TT_Prepare('translation', 'bc08_vri01_nfl', '_bc08_nfl', 'ab06_avi01_nfl');
 ------------------------
 --DROP TABLE IF EXISTS casfri50.nfl_all;
-CREATE TABLE casfri50.nfl_all AS -- 2m14s -- 1m53s
+CREATE TABLE casfri50.nfl_all AS -- 2m24s
 SELECT * FROM TT_Translate_ab06_nfl('rawfri', 'ab06', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab06_avi01_nfl');
 ------------------------
-INSERT INTO casfri50.nfl_all -- 23m3s -- 18m21s
+INSERT INTO casfri50.nfl_all -- 23m43s
 SELECT * FROM TT_Translate_ab16_nfl('rawfri', 'ab16', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab16_avi01_nfl');
 ------------------------
-INSERT INTO casfri50.nfl_all -- 1h21m -- 45m45s
+INSERT INTO casfri50.nfl_all -- 1h4m
 SELECT * FROM TT_Translate_nb01_nfl('rawfri', 'nb01', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'nb01_nbi01_nfl');
 ------------------------
-INSERT INTO casfri50.nfl_all -- 13h43m --12h36m
+INSERT INTO casfri50.nfl_all -- 16h38m
 SELECT * FROM TT_Translate_bc08_nfl('rawfri', 'bc08', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'bc08_vri01_nfl');
@@ -201,27 +201,26 @@ SELECT TT_Prepare('translation', 'nb01_nbi01_geo', '_nb01_geo', 'ab06_avi01_geo'
 SELECT TT_Prepare('translation', 'bc08_vri01_geo', '_bc08_geo', 'ab06_avi01_geo');
 ------------------------
 --DROP TABLE IF EXISTS casfri50.geo_all;
-CREATE TABLE casfri50.geo_all AS
-SELECT * FROM TT_Translate_ab06_geo('rawfri', 'ab06', 'ogc_fid'); -- 54 secs
+CREATE TABLE casfri50.geo_all AS -- 54s
+SELECT * FROM TT_Translate_ab06_geo('rawfri', 'ab06', 'ogc_fid'); 
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab06_avi01_geo');
 ------------------------
-INSERT INTO casfri50.geo_all 
-SELECT * FROM TT_Translate_ab16_geo('rawfri', 'ab16', 'ogc_fid'); -- 7 mins 54 secs
+INSERT INTO casfri50.geo_all -- 7m30s
+SELECT * FROM TT_Translate_ab16_geo('rawfri', 'ab16', 'ogc_fid'); 
 
 SELECT * FROM TT_ShowLastLog('translation', 'ab16_avi01_geo');
 ------------------------
-INSERT INTO casfri50.geo_all 
-SELECT * FROM TT_Translate_nb01_geo('rawfri', 'nb01', 'ogc_fid'); -- 50 mins 21 secs
+INSERT INTO casfri50.geo_all -- 48m52s
+SELECT * FROM TT_Translate_nb01_geo('rawfri', 'nb01', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'nb01_nbi01_geo');
 ------------------------
-INSERT INTO casfri50.geo_all 
-SELECT * FROM TT_Translate_bc08_geo('rawfri', 'bc08', 'ogc_fid'); -- 5 hrs 25 mins
+INSERT INTO casfri50.geo_all --4h59m
+SELECT * FROM TT_Translate_bc08_geo('rawfri', 'bc08', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'bc08_vri01_geo');
 ------------------------
 SELECT count(*) FROM casfri50.geo_all; --5736548
 SELECT count(*) FROM casfri50.geo_all WHERE geometry = '010300000000000000'; -- 0
 ------------------------
-
