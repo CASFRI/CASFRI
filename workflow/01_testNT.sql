90af4a11578e1a65638e6920b81ac2a05d56360b
Pierre R Vernier <pierre.vernier@gmail.com>
2019-11-05 14:53:50 -0500

    Added csv files for NT translation

---
+++ workflow/01_testNT.sql
@@ -0,0 +1,208 @@
+------------------------------------------------------------------------------
+-- CASFRI Sample workflow file for CASFRI v5 beta
+-- For use with PostgreSQL Table Tranlation Engine v0.1 for PostgreSQL 9.x
+-- https://github.com/edwardsmarc/postTranslationEngine
+-- https://github.com/edwardsmarc/casfri
+--
+-- This is free software; you can redistribute and/or modify it under
+-- the terms of the GNU General Public Licence. See the COPYING file.
+--
+-- Copyright (C) 2018-2020 Pierre Racine <pierre.racine@sbf.ulaval.ca>, 
+--                         Marc Edwards <medwards219@gmail.com>,
+--                         Pierre Vernier <pierre.vernier@gmail.com>
+-------------------------------------------------------------------------------
+
+-- No not display debug messages.
+SET tt.debug TO TRUE;
+SET tt.debug TO FALSE;
+
+-------------------------------------------------------
+-------------------------------------------------------
+-- Create 200 random rows views on each source inventory
+-------------------------------------------------------
+-------------------------------------------------------
+-- NT01
+-------------------------------------------------------
+-- display one of the source inventory table
+SELECT * FROM rawfri.nt01 LIMIT 10;
+
+-- count the number of rows
+SELECT count(*)
+FROM rawfri.nt01;
+
+-- create a 200 rows test inventory table
+--DROP VIEW IF EXISTS rawfri.nt01_test_200;
+CREATE OR REPLACE VIEW rawfri.nt01_test_200 AS
+SELECT invproj_id, fc_id_1, ogc_fid, wkb_geometry, areaha, 
+       moisture, crownclos, height, 
+       sp1, sp1_per, sp2, sp2per, sp3, sp3per, sp4, sp4per, 
+       structur, strc_per, origin, typeclas, 
+       dis1code, dis1year, dis1ext
+FROM rawfri.nt01 TABLESAMPLE SYSTEM (300.0*100/11484) REPEATABLE (1.0)
+--WHERE ogc_fid = 2
+LIMIT 200;
+
+-- display
+SELECT * FROM rawfri.nt01_test_200;
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- Create test translation tables
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+CREATE SCHEMA translation_test;
+
+-------------------------------------------------------
+-- NT01
+-------------------------------------------------------
+-- display translation tables
+SELECT * FROM translation.nt01_fvi01_cas; 
+SELECT * FROM translation.nt01_fvi01_dst; 
+SELECT * FROM translation.nt01_fvi01_eco; 
+SELECT * FROM translation.nt01_fvi01_lyr; 
+SELECT * FROM translation.nt01_fvi01_nfl;
+SELECT * FROM translation.nt01_fvi01_geo;
+----------------------------
+-- create subsets of translation tables if necessary
+----------------------------
+-- cas
+DROP TABLE IF EXISTS translation_test.nt01_fvi01_cas_test;
+CREATE TABLE translation_test.nt01_fvi01_cas_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_cas
+WHERE rule_id::int < 10
+;
+-- display
+SELECT * FROM translation_test.nt01_fvi01_cas_test;
+----------------------------
+-- dst
+DROP TABLE IF EXISTS translation_test.nt01_fvi01_dst_test;
+CREATE TABLE translation_test.nt01_fvi01_dst_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_dst
+--WHERE rule_id::int = 1
+;
+-- display
+SELECT * FROM translation_test.nt01_fvi01_dst_test;
+----------------------------
+-- eco
+DROP TABLE IF EXISTS translation_test.nt01_fvi01_eco_test;
+CREATE TABLE translation_test.nt01_fvi01_eco_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_eco
+--WHERE rule_id::int = 1
+;
+-- display
+SELECT * FROM translation_test.nt01_fvi01_eco_test;
+----------------------------
+-- lyr
+DROP TABLE IF EXISTS translation_test.nt01_fvi01_lyr_test;
+CREATE TABLE translation_test.nt01_fvi01_lyr_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_lyr
+--WHERE rule_id::int = 1
+;
+-- display
+SELECT * FROM translation_test.nt01_fvi01_lyr_test;
+----------------------------
+-- nfl
+DROP TABLE IF EXISTS translation_test.nt01_fvi01_nfl_test;
+CREATE TABLE translation_test.nt01_fvi01_nfl_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_nfl
+--WHERE rule_id::int = 1
+;
+-- display
+SELECT * FROM translation_test.nt01_fvi01_nfl_test;
+----------------------------
+-- geo
+DROP TABLE IF EXISTS translation_test.nt01_fvi01_geo_test;
+CREATE TABLE translation_test.nt01_fvi01_geo_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_geo
+--WHERE rule_id::int = 2
+;
+-- display
+SELECT * FROM translation_test.nt01_fvi01_geo_test;
+
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- Validate dependency tables
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- NT species table
+-------------------------------------------------------
+SELECT TT_Prepare('translation', 'nt_fvi01_species_validation', '_nt_species_val');
+SELECT * FROM TT_Translate_nt_species_val('translation', 'nt_fvi01_species');
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- Translate the sample table
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-- NT01
+-------------------------------------------------------
+-- create translation functions
+SELECT TT_Prepare('translation_test', 'nt01_fvi01_cas_test', '_nt01_cas');
+SELECT TT_Prepare('translation_test', 'nt01_fvi01_dst_test', '_nt01_dst');
+SELECT TT_Prepare('translation_test', 'nt01_fvi01_eco_test', '_nt01_eco');
+SELECT TT_Prepare('translation_test', 'nt01_fvi01_lyr_test', '_nt01_lyr');
+SELECT TT_Prepare('translation_test', 'nt01_fvi01_nfl_test', '_nt01_nfl');
+SELECT TT_Prepare('translation_test', 'nt01_fvi01_geo_test', '_nt01_geo');
+
+-- translate the samples (5 sec.)
+SELECT * FROM TT_Translate_nt01_cas('rawfri', 'nt01_test_200', 'ogc_fid'); -- 6 s.
+SELECT * FROM TT_ShowLastLog('translation_test', 'nt01_fvi01_cas_test');
+
+-- display original values and translated values side-by-side to compare and debug the translation table
+SELECT src_filename, trm_1, poly_num, cas_id, 
+       density, crown_closure_lower, crown_closure_upper, 
+       height, height_upper, height_lower,
+       sp1, species_1,
+       sp1_per, species_per_1
+FROM TT_Translate_nt01_lyr('rawfri', 'nt01_test_200'), rawfri.nt01_test_200
+WHERE poly_num = substr(cas_id, 33, 10)::int;
+
+--------------------------------------------------------------------------
+SELECT TT_DeleteAllLogs('translation_test');
+
+
+-------------------------------------------------------
+-- NT02
+-------------------------------------------------------
+-- create translation tables from NT01 tables
+----------------------------
+-- cas
+DROP TABLE IF EXISTS translation_test.nt02_fvi01_cas_test;
+CREATE TABLE translation_test.nt02_fvi01_cas_test WITH OIDS AS
+SELECT * FROM translation.nt01_fvi01_cas;
+-- Update cas_id
+UPDATE translation_test.nt02_fvi01_cas_test
+SET translation_rules = regexp_replace(translation_rules, 'nt01', 'nt02')
+WHERE rule_id = '1';
+-- change 'Forest' to 'geonb_forest-foret' in validation rules
+UPDATE translation_test.nt02_fvi01_cas_test
+SET validation_rules = regexp_replace(validation_rules, 'fc_id_1', 'fc_id')
+WHERE rule_id IN ('1');
+-- display
+SELECT * FROM translation_test.nt02_fvi01_cas_test;
+----------------------------
+
+-- create a 200 rows test inventory table
+--DROP VIEW IF EXISTS rawfri.nt02_test_200;
+CREATE OR REPLACE VIEW rawfri.nt02_test_200 AS
+SELECT invproj_id, fc_id, ogc_fid, geom, areaha, 
+       moisture, crownclos, height, 
+       sp1, sp1_per, sp2, sp2per, sp3, sp3per, sp4, sp4per, 
+       structur, strc_per, origin, typeclas, 
+       dis1code, dis1year, dis1ext
+FROM rawfri.nt02 TABLESAMPLE SYSTEM (300.0*100/11484) REPEATABLE (1.0)
+--WHERE ogc_fid = 2
+LIMIT 200;
+
+-- display
+SELECT * FROM rawfri.nt02_test_200;
+--------------------------------------------------------------------------
+
+SELECT TT_Prepare('translation_test', 'nt02_fvi01_cas_test', '_nt02_cas');
+
+SELECT * FROM TT_Translate_nt02_cas('rawfri', 'nt02_test_200', 'ogc_fid'); -- 5 s.
+SELECT * FROM TT_ShowLastLog('translation_test', 'nt02_nbi01_cas_test');
