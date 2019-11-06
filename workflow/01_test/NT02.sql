fcf00bd79301ecfdd2f10ccd63e4a2c1a3b4dfbd
Pierre R Vernier <pierre.vernier@gmail.com>
2019-11-06 17:17:46 -0500

    Split NT in 2 inventories

--- workflow/01_test/NT02.sql
+++ workflow/01_test/NT02.sql
@@ -11,10 +11,22 @@ SELECT * FROM translation.nt01_fvi01_cas;
 UPDATE translation_test.nt02_fvi01_cas_test
 SET translation_rules = regexp_replace(translation_rules, 'nt01', 'nt02')
 WHERE rule_id = '1';
--- change 'Forest' to 'geonb_forest-foret' in validation rules
+-- change 'fc_id_1' to 'fc_id' in validation rules
 UPDATE translation_test.nt02_fvi01_cas_test
-SET validation_rules = regexp_replace(validation_rules, 'fc_id_1', 'fc_id')
-WHERE rule_id IN ('1');
+SET validation_rules = regexp_replace(validation_rules, 'fc_id_1', 'fc_id', 'g')
+WHERE rule_id IN ('1','2');
+-- change 'fc_id_1' to 'fc_id' in translation rules
+UPDATE translation_test.nt02_fvi01_cas_test
+SET translation_rules = regexp_replace(translation_rules, 'fc_id_1', 'fc_id')
+WHERE rule_id IN ('1','2');
+-- change 'wkb_geometry' to 'geom' in validation rules
+UPDATE translation_test.nt02_fvi01_cas_test
+SET validation_rules = regexp_replace(validation_rules, 'wkb_geometry', 'geom')
+WHERE rule_id IN ('7','8');
+-- change 'wkb_geometry' to 'geom' in translation rules
+UPDATE translation_test.nt02_fvi01_cas_test
+SET translation_rules = regexp_replace(translation_rules, 'wkb_geometry', 'geom')
+WHERE rule_id IN ('7','8');
 -- display
 SELECT * FROM translation_test.nt02_fvi01_cas_test;
 ----------------------------
@@ -25,7 +37,7 @@ CREATE OR REPLACE VIEW rawfri.nt02_test_200 AS
 SELECT invproj_id, fc_id, ogc_fid, geom, areaha, 
        moisture, crownclos, height, 
        sp1, sp1_per, sp2, sp2per, sp3, sp3per, sp4, sp4per, 
-       structur, strc_per, origin, typeclas, 
+       ref_year, structur, strc_per, origin, typeclas, 
        dis1code, dis1year, dis1ext
 FROM rawfri.nt02 TABLESAMPLE SYSTEM (300.0*100/11484) REPEATABLE (1.0)
 --WHERE ogc_fid = 2
@@ -35,7 +47,9 @@ LIMIT 200;
 SELECT * FROM rawfri.nt02_test_200;
 --------------------------------------------------------------------------
 
+-- Create translation function
 SELECT TT_Prepare('translation_test', 'nt02_fvi01_cas_test', '_nt02_cas');
 
-SELECT * FROM TT_Translate_nt02_cas('rawfri', 'nt02_test_200', 'ogc_fid'); -- 5 s.
+-- Translate the samples (reuse most of NB01 translation functions)
+SELECT * FROM TT_Translate_nt02_cas('rawfri', 'nt02_test_200', 'ogc_fid');
 SELECT * FROM TT_ShowLastLog('translation_test', 'nt02_nbi01_cas_test');
