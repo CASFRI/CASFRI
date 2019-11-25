5107cc06ade0805a168dd00f0b024d3e8e58a84e
Pierre <pierre.racine@sbf.ulaval.ca>
2019-11-25 15:04:26 -0500

    Progressive update to translation/test/
    Ordered new table

--- translation/test/dump_test_tables.sh
+++ translation/test/dump_test_tables.sh
@@ -15,12 +15,12 @@ source ../../conversion/sh/common.sh
 
 rm ./data/*.csv
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data\cas_all_test.csv" "$pg_connection_string" "casfri50_test.cas_all_new"
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data\cas_all_test.csv" "$pg_connection_string" "casfri50_test.cas_all_new_ordered"
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data\dst_all_test.csv" "$pg_connection_string" "casfri50_test.dst_all_new" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data\dst_all_test.csv" "$pg_connection_string" "casfri50_test.dst_all_new_ordered" 
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data\eco_all_test.csv" "$pg_connection_string" "casfri50_test.eco_all_new" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data\eco_all_test.csv" "$pg_connection_string" "casfri50_test.eco_all_new_ordered" 
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data\lyr_all_test.csv" "$pg_connection_string" "casfri50_test.lyr_all_new" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data\lyr_all_test.csv" "$pg_connection_string" "casfri50_test.lyr_all_new_ordered" 
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data\nfl_all_test.csv" "$pg_connection_string" "casfri50_test.nfl_all_new" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data\nfl_all_test.csv" "$pg_connection_string" "casfri50_test.nfl_all_new_ordered" 
