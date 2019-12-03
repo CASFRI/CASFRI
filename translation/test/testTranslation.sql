38981404e6ce8a927d6b97e8b7fdae9bc8e8c448
Pierre <pierre.racine@sbf.ulaval.ca>
2019-12-03 10:09:01 -0500

    Removed duplicate line in the instructions.

--- translation/test/testTranslation.sql
+++ translation/test/testTranslation.sql
@@ -15,7 +15,6 @@
 --
 -- 1) Load the test tables from a command window with load_test_tables.bat.
 -- 2) Execute "test_translation.sql" in PostgreSQL (this file).
--- 2) Execute "test_translation.sql" in PostgreSQL (this file)
 -- 3) If undesirable changes show up, fix your translation tables.
 --    If desirable changes occurs, dump them as new test tables with 
 --    dump_test_tables.bat and commit.
