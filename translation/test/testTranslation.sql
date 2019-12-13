4f20036d14c85534ac7144ce1e5d7f5b01b041d1
Pierre R Vernier <pierre.vernier@gmail.com>
2019-12-13 10:12:22 -0500

    Minor edit to testTranslation

--- translation/test/testTranslation.sql
+++ translation/test/testTranslation.sql
@@ -250,9 +250,9 @@ SELECT TT_CreateMappingView('rawfri', 'nb01', 'nb', 600, 'NFL');
 INSERT INTO casfri50_test.nfl_all_new
 SELECT * FROM TT_Translate_nb01_nfl_test('rawfri', 'nb01_l1_to_nb_l1_map_600_nfl');
 ------------------------
-SELECT TT_CreateMappingView('rawfri', 'nb01', 2, 'nb', 1, 600, 'NFL');
+SELECT TT_CreateMappingView('rawfri', 'nb02', 'nb', 600, 'NFL');
 INSERT INTO casfri50_test.nfl_all_new
-SELECT * FROM TT_Translate_nb01_nfl_test('rawfri', 'nb01_l2_to_nb_l1_map_600_nfl');
+SELECT * FROM TT_Translate_nb01_nfl_test('rawfri', 'nb02_l1_to_nb_l1_map_600_nfl');
 ------------------------
 SELECT TT_CreateMappingView('rawfri', 'bc08', 1000, 'NFL');
 INSERT INTO casfri50_test.nfl_all_new
