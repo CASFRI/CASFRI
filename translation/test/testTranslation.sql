9c7b9c66857ebd4a3571d952d69b57e1761f9fb5
Pierre R Vernier <pierre.vernier@gmail.com>
2019-12-13 11:09:28 -0500

    Push old tables back

--- translation/test/testTranslation.sql
+++ translation/test/testTranslation.sql
@@ -256,7 +256,7 @@ SELECT * FROM TT_Translate_nb01_nfl_test('rawfri', 'nb02_l1_to_nb_l1_map_600_nfl
 ------------------------
 SELECT TT_CreateMappingView('rawfri', 'bc08', 1000, 'NFL');
 INSERT INTO casfri50_test.nfl_all_new
-SELECT * FROM TT_Translate_bc08_nfl_test('rawfri', 'bc08_min_1000_bc08_nfl');
+SELECT * FROM TT_Translate_bc08_nfl_test('rawfri', 'bc08_min_1000_nfl');
 ------------------------
 SELECT TT_CreateMappingView('rawfri', 'nt01', 'nt', 500, 'NFL');
 INSERT INTO casfri50_test.nfl_all_new
