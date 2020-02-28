f39d58d3e5019ce75de6e339e3c2a3f34dde3fb4
Marc <medwards219@gmail.com>
2020-02-27 19:18:07 -0500

    Edit to bat file.  #270

--- conversion/bat/load_yt02.bat
+++ conversion/bat/load_yt02.bat
@@ -34,3 +34,5 @@ SET fullTargetTableName=%targetFRISchema%.yt02
 -s_srs "+proj=aea +lat_1=61.66666666666666 +lat_2=68 +lat_0=59 +lon_0=-132.5 +x_0=500000 +y_0=500000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs" ^
 -sql "SELECT *, '%srcFileName%' AS src_filename, '%inventoryID%' AS inventory_id FROM '%srcFileName%'" ^
 -progress %overwrite_tab%
+
+ENDLOCAL
