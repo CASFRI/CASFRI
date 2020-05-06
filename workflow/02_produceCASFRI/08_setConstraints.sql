c277a69c64fb250de63ae0b2a01a755fe5bdd24e
Pierre <pierre.racine@sbf.ulaval.ca>
2020-05-06 18:48:27 -0400

    #300 Added more constraints on CAS and DST tables

--- workflow/02_produceCASFRI/08_setConstraints.sql
+++ workflow/02_produceCASFRI/08_setConstraints.sql
@@ -11,6 +11,10 @@
 --                         Marc Edwards <medwards219@gmail.com>,
 --                         Pierre Vernier <pierre.vernier@gmail.com>
 -------------------------------------------------------------------------------
+-- Create a schema for lookup tables
+-------------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS casfri50_lookup ;
+-------------------------------------------------------
 -- Add different constraints to the CAS_ALL table
 -------------------------------------------------------
 -- Add foreign key to HDR_ALL
@@ -50,29 +54,66 @@ ALTER TABLE casfri50.cas_all
 ALTER COLUMN stand_photo_year SET NOT NULL;
 
 -- Ensure CAS table CAS_ID is 50 characters long
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS cas_id_length;
 ALTER TABLE casfri50.cas_all
 ADD CONSTRAINT cas_id_length
 CHECK (length(cas_id) = 50);
 
 -- Ensure CAS table INVENTORY_ID is 4 characters long
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS inventory_id_length;
 ALTER TABLE casfri50.cas_all
 ADD CONSTRAINT inventory_id_length
 CHECK (length(inventory_id) = 4);
 
 -- Ensure CAS table NUM_OF_LAYERS is greater than 0
--- BUG!
+-- Issue on replacing 0 with UNKNOWN_VALUE
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS num_of_layers_greater_than_zero;
 ALTER TABLE casfri50.cas_all
 ADD CONSTRAINT num_of_layers_greater_than_zero
-CHECK (num_of_layers > 0);
+CHECK (num_of_layers > 0 OR num_of_layers = -8886);
+
+-- Ensure CAS table STAND_STRUCTURE values match the corresponding lookup table
+DROP TABLE IF EXISTS casfri50_lookup.stand_structure_codes;
+CREATE TABLE casfri50_lookup.stand_structure_codes AS
+SELECT * FROM (VALUES ('S'), ('M'), ('H'), ('C'), ('NULL_VALUE'), ('EMPTY_STRING'), ('NOT_IN_SET')) AS t(code);
+
+ALTER TABLE casfri50_lookup.stand_structure_codes
+ADD PRIMARY KEY (code);
+
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS cas_stand_structure_fk;
+ALTER TABLE casfri50.cas_all
+ADD CONSTRAINT cas_stand_structure_fk FOREIGN KEY (stand_structure) 
+REFERENCES casfri50_lookup.stand_structure_codes (code);
 
 -- Ensure CAS table STAND_STRUCTURE fits with NUM_OF_LAYERS
--- BUG!
+-- Issue on replacing 0 with UNKNOWN_VALUE
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS stand_structure_num_of_layers;
 ALTER TABLE casfri50.cas_all
 ADD CONSTRAINT stand_structure_num_of_layers 
-CHECK (stand_structure = 'M' AND num_of_layers > 1);
+CHECK (((stand_structure = 'M' OR stand_structure = 'H' OR stand_structure = 'C') AND num_of_layers > 1) OR 
+       ((stand_structure = 'S' OR stand_structure = 'C') AND num_of_layers = 1) OR num_of_layers = -8886);
+
+-- Ensure CAS table CASFRI_AREA is greater than 0
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS casfri_area_greater_than_zero;
+ALTER TABLE casfri50.cas_all
+ADD CONSTRAINT casfri_area_greater_than_zero
+CHECK (casfri_area > 0);
+
+-- Ensure CAS table CASFRI_PERIMETER is greater than 0
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS casfri_perimeter_greater_than_zero;
+ALTER TABLE casfri50.cas_all
+ADD CONSTRAINT casfri_perimeter_greater_than_zero
+CHECK (casfri_perimeter > 0);
+
+-- Ensure CAS table SRC_INV_AREA is greater than 0
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS src_inv_area_greater_than_zero;
+ALTER TABLE casfri50.cas_all
+ADD CONSTRAINT src_inv_area_greater_than_zero
+CHECK (src_inv_area >= 0 OR src_inv_area = -8887);
 
 -- Ensure CAS table STAND_PHOTO_YEAR is greater than 0
--- BUG!
+-- Issue #248
+ALTER TABLE casfri50.cas_all DROP CONSTRAINT IF EXISTS stand_photo_year_greater_than_zero;
 ALTER TABLE casfri50.cas_all
 ADD CONSTRAINT stand_photo_year_greater_than_zero
 CHECK (stand_photo_year > 0);
@@ -128,14 +169,113 @@ ALTER TABLE casfri50.dst_all
 ALTER COLUMN layer SET NOT NULL;
 
 -- Ensure DST table CAS_ID is 50 characters long
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS cas_id_length;
 ALTER TABLE casfri50.dst_all
 ADD CONSTRAINT cas_id_length
 CHECK (length(cas_id) = 50);
 
 -- Ensure DST table LAYER is greater than 0
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS layer_greater_than_zero;
 ALTER TABLE casfri50.dst_all
 ADD CONSTRAINT layer_greater_than_zero
-CHECK (layer > 0);
+CHECK (layer > 0 OR layer = -8886);
+
+-- Ensure DST table DIST_TYPE_1 values match the corresponding lookup table
+DROP TABLE IF EXISTS casfri50_lookup.dist_type_1_codes CASCADE;
+CREATE TABLE casfri50_lookup.dist_type_1_codes AS
+SELECT * FROM (VALUES ('CO'), ('PC'), ('BU'), ('WF'), ('DI'), ('IK'), ('FL'), 
+                      ('WE'), ('SL'), ('OT'), ('DT'), ('SI'), ('CL'), ('UK'),
+                      ('NULL_VALUE'), ('EMPTY_STRING'), ('NOT_IN_SET')) AS t(code);
+
+ALTER TABLE casfri50_lookup.dist_type_1_codes
+ADD PRIMARY KEY (code);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dst_dist_type_1_fk;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dst_dist_type_1_fk FOREIGN KEY (dist_type_1) 
+REFERENCES casfri50_lookup.dist_type_1_codes (code);
+
+-- Ensure DST table DIST_TYPE_2 and DIST_TYPE_3 values match the corresponding lookup table
+DROP TABLE IF EXISTS casfri50_lookup.dist_type_2_3_codes CASCADE;
+CREATE TABLE casfri50_lookup.dist_type_2_3_codes AS
+SELECT * FROM (VALUES ('CO'), ('PC'), ('BU'), ('WF'), ('DI'), ('IK'), ('FL'), 
+                      ('WE'), ('SL'), ('OT'), ('DT'), ('SI'), ('CL'), ('UK'),
+                      ('NULL_VALUE'), ('EMPTY_STRING'), ('NOT_IN_SET'), ('NOT_APPLICABLE')) AS t(code);
+
+ALTER TABLE casfri50_lookup.dist_type_2_3_codes
+ADD PRIMARY KEY (code);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dst_dist_type_2_fk;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dst_dist_type_2_fk FOREIGN KEY (dist_type_2) 
+REFERENCES casfri50_lookup.dist_type_2_3_codes (code);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dst_dist_type_3_fk;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dst_dist_type_3_fk FOREIGN KEY (dist_type_3) 
+REFERENCES casfri50_lookup.dist_type_2_3_codes (code);
+
+-- Ensure DST table DIST_YEAR_1, DIST_YEAR_2 and DIST_YEAR_2 are greater than 1900 and below 2020
+-- Issue #337
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_year_1_greater_than_1900;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_year_1_greater_than_1900
+CHECK ((1900 <= dist_year_1 AND dist_year_1 <= 2020) OR 
+       dist_year_1 = -9999 OR dist_year_1 = -8888);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_year_2_greater_than_1900;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_year_2_greater_than_1900
+CHECK ((1900 <= dist_year_2 AND dist_year_2 <= 2020) OR 
+       dist_year_2 = -9999 OR dist_year_2 = -8888 OR dist_year_2 = -8887);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_year_3_greater_than_1900;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_year_3_greater_than_1900
+CHECK ((1900 <= dist_year_3 AND dist_year_3 <= 2020) OR 
+       dist_year_3 = -9999 OR dist_year_3 = -8888 OR dist_year_3 = -8887);
+
+-- Ensure DST table DIST_EXT_UPPER_1, DIST_EXT_UPPER_2 and DIST_EXT_UPPER_3 
+-- are greater than 10 and below 100
+-- Issue #338
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_ext_upper_1_betweeen_10_and_100;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_ext_upper_1_betweeen_10_and_100
+CHECK ((10 <= dist_ext_upper_1 AND dist_ext_upper_1 <= 100) OR 
+       dist_ext_upper_1 = -9999 OR dist_ext_upper_1 = -8888 OR dist_ext_upper_1 = -8887);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_ext_upper_2_betweeen_10_and_100;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_ext_upper_2_betweeen_10_and_100
+CHECK ((10 <= dist_ext_upper_2 AND dist_ext_upper_2 <= 100) OR 
+       dist_ext_upper_2 = -9999 OR dist_ext_upper_2 = -8888 OR dist_ext_upper_2 = -8887);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_ext_upper_3_betweeen_10_and_100;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_ext_upper_3_betweeen_10_and_100
+CHECK ((10 <= dist_ext_upper_3 AND dist_ext_upper_3 <= 100) OR 
+       dist_ext_upper_3 = -9999 OR dist_ext_upper_3 = -8888 OR dist_ext_upper_3 = -8887);
+       
+-- Ensure DST table DIST_EXT_LOWER_1, DIST_EXT_LOWER_2 and DIST_EXT_LOWER_3 
+-- are greater than 10 and below 100
+-- Issue #338
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_ext_lower_1_betweeen_10_and_100;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_ext_lower_1_betweeen_10_and_100
+CHECK ((10 <= dist_ext_lower_1 AND dist_ext_lower_1 <= 100) OR 
+       dist_ext_lower_1 = -9999 OR dist_ext_lower_1 = -8888 OR dist_ext_lower_1 = -8887);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_ext_lower_2_betweeen_10_and_100;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_ext_lower_2_betweeen_10_and_100
+CHECK ((10 <= dist_ext_lower_2 AND dist_ext_lower_2 <= 100) OR 
+       dist_ext_lower_2 = -9999 OR dist_ext_lower_2 = -8888 OR dist_ext_lower_2 = -8887);
+
+ALTER TABLE casfri50.dst_all DROP CONSTRAINT IF EXISTS dist_ext_lower_3_betweeen_10_and_100;
+ALTER TABLE casfri50.dst_all
+ADD CONSTRAINT dist_ext_lower_3_betweeen_10_and_100
+CHECK ((10 <= dist_ext_lower_3 AND dist_ext_lower_3 <= 100) OR 
+       dist_ext_lower_3 = -9999 OR dist_ext_lower_3 = -8888 OR dist_ext_lower_3 = -8887);
 
 -------------------------------------------------------
 -- Add different constraints to the ECO_ALL table
