184c9d5c60f4f65ef992ae953477a83600499fd6
Pierre <pierre.racine@sbf.ulaval.ca>
2020-05-29 15:12:52 -0400

    VACUUM ANALYZE loaded inventories to make sure PG planner use the right statistics

--- conversion/bat/common_postprocessing.bat
+++ conversion/bat/common_postprocessing.bat
@@ -7,3 +7,6 @@ IF %createSQLSpatialIndex%x == Truex (
 
 :: Create an index on OGC_FID
 "%gdalFolder%/ogrinfo" %pg_connection_string% -sql "CREATE INDEX ON %fullTargetTableName% (ogc_fid)";
+
+:: VACUUM ANALYZE the table
+"%gdalFolder%/ogrinfo" %pg_connection_string% -sql "VACUUM ANALYZE %fullTargetTableName%";
