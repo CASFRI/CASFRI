b226078c9de1b1ec282801da368b351640f8a070
Pierre <pierre.racine@sbf.ulaval.ca>
2020-06-09 15:57:24 -0400

    Update to file header

--- helperfunctions/helperFunctionsCASFRITest.sql
+++ helperfunctions/helperFunctionsCASFRITest.sql
@@ -1,15 +1,15 @@
 ------------------------------------------------------------------------------
--- PostgreSQL Table Tranlation Engine - Test file
--- Version 0.1 for PostgreSQL 9.x
+-- CASFRI Helper functions test file for CASFR v5 beta
+-- For use with PostgreSQL Table Tranlation Engine v0.1 for PostgreSQL 9.x
 -- https://github.com/edwardsmarc/postTranslationEngine
+-- https://github.com/edwardsmarc/casfri
 --
 -- This is free software; you can redistribute and/or modify it under
 -- the terms of the GNU General Public Licence. See the COPYING file.
 --
--- Copyright (C) 2018-2020 Pierre Racine <pierre.racine@sbf.ulaval.ca>,
+-- Copyright (C) 2018-2020 Pierre Racine <pierre.racine@sbf.ulaval.ca>, 
 --                         Marc Edwards <medwards219@gmail.com>,
 --                         Pierre Vernier <pierre.vernier@gmail.com>
---
 -------------------------------------------------------------------------------
 SET lc_messages TO 'en_US.UTF-8';
 
