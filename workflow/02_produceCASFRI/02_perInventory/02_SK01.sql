088bd0ddd76ad44e4ad9759744e1ce04dbd61552
Pierre <pierre.racine@sbf.ulaval.ca>
2020-07-14 18:00:58 -0400

    Added suffixes to TT_Prepare() and TT_Translate() to avoid locks when translating in parallel

--- workflow/02_produceCASFRI/02_perInventory/02_SK01.sql
+++ workflow/02_produceCASFRI/02_perInventory/02_SK01.sql
@@ -19,7 +19,7 @@ SET tt.debug TO FALSE;
 --------------------------------------------------------------------------
 -- CAS
 ------------------------
-SELECT TT_Prepare('translation', 'sk_utm01_cas', '_sk_cas', 'ab_avi01_cas'); 
+SELECT TT_Prepare('translation', 'sk_utm01_cas', '_sk01_cas', 'ab_avi01_cas'); 
 
 SELECT TT_CreateMappingView('rawfri', 'sk01', 'sk_utm');
 
@@ -28,14 +28,14 @@ DELETE FROM casfri50.cas_all WHERE left(cas_id, 4) = 'SK01';
 
 -- Add translated ones
 INSERT INTO casfri50.cas_all -- 
-SELECT * FROM TT_Translate_sk_cas('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
+SELECT * FROM TT_Translate_sk01_cas('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_cas', 'sk01_l1_to_sk_utm_l1_map');
 
 ------------------------
 -- DST
 ------------------------
-SELECT TT_Prepare('translation', 'sk_utm01_dst', '_sk_dst', 'ab_avi01_dst');
+SELECT TT_Prepare('translation', 'sk_utm01_dst', '_sk01_dst', 'ab_avi01_dst');
 
 SELECT TT_CreateMappingView('rawfri', 'sk01', 1, 'sk_utm', 1);
 
@@ -44,14 +44,14 @@ DELETE FROM casfri50.dst_all WHERE left(cas_id, 4) = 'SK01';
 
 -- Add translated ones
 INSERT INTO casfri50.dst_all -- 
-SELECT * FROM TT_Translate_sk_dst('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
+SELECT * FROM TT_Translate_sk01_dst('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_dst', 'sk01_l1_to_sk_utm_l1_map');
 
 ------------------------
 -- ECO
 ------------------------
-SELECT TT_Prepare('translation', 'sk_utm01_eco', '_sk_eco', 'ab_avi01_eco');
+SELECT TT_Prepare('translation', 'sk_utm01_eco', '_sk01_eco', 'ab_avi01_eco');
 
 SELECT TT_CreateMappingView('rawfri', 'sk01', 'sk_utm');
 
@@ -60,7 +60,7 @@ DELETE FROM casfri50.eco_all WHERE left(cas_id, 4) = 'SK01';
 
 -- Add translated ones
 INSERT INTO casfri50.eco_all -- 
-SELECT * FROM TT_Translate_sk_eco('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
+SELECT * FROM TT_Translate_sk01_eco('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_eco', 'sk01_l1_to_sk_utm_l1_map');
 
@@ -73,7 +73,7 @@ ON translation.species_code_mapping (sk_species_codes)
 WHERE TT_NotEmpty(sk_species_codes);
 
 -- Prepare the translation function
-SELECT TT_Prepare('translation', 'sk_utm01_lyr', '_sk_lyr', 'ab_avi01_lyr'); 
+SELECT TT_Prepare('translation', 'sk_utm01_lyr', '_sk01_lyr', 'ab_avi01_lyr'); 
 
 -- Delete existing entries
 DELETE FROM casfri50.lyr_all WHERE left(cas_id, 4) = 'SK01';
@@ -84,7 +84,7 @@ DELETE FROM casfri50.lyr_all WHERE left(cas_id, 4) = 'SK01';
 SELECT TT_CreateMappingView('rawfri', 'sk01', 1, 'sk_utm', 1);
 
 INSERT INTO casfri50.lyr_all -- 
-SELECT * FROM TT_Translate_sk_lyr('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
+SELECT * FROM TT_Translate_sk01_lyr('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
 
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_lyr', 'sk01_l1_to_sk_utm_l1_map');
@@ -94,14 +94,14 @@ SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_lyr', 'sk01_l1_to_sk_utm_l
 SELECT TT_CreateMappingView('rawfri', 'sk01', 2, 'sk_utm', 1);
 
 INSERT INTO casfri50.lyr_all -- 
-SELECT * FROM TT_Translate_sk_lyr('rawfri', 'sk01_l2_to_sk_utm_l1_map', 'ogc_fid');
+SELECT * FROM TT_Translate_sk01_lyr('rawfri', 'sk01_l2_to_sk_utm_l1_map', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_lyr', 'sk01_l2_to_sk_utm_l1_map');
 
 ------------------------
 -- NFL
 ------------------------
-SELECT TT_Prepare('translation', 'sk_utm01_nfl', '_sk_nfl', 'ab_avi01_nfl');
+SELECT TT_Prepare('translation', 'sk_utm01_nfl', '_sk01_nfl', 'ab_avi01_nfl');
 
 SELECT TT_CreateMappingView('rawfri', 'sk01', 3, 'sk_utm', 1);
 
@@ -110,14 +110,14 @@ DELETE FROM casfri50.nfl_all WHERE left(cas_id, 4) = 'SK01';
 
 -- Add translated ones
 INSERT INTO casfri50.nfl_all -- 
-SELECT * FROM TT_Translate_sk_nfl('rawfri', 'sk01_l3_to_sk_utm_l1_map', 'ogc_fid'); 
+SELECT * FROM TT_Translate_sk01_nfl('rawfri', 'sk01_l3_to_sk_utm_l1_map', 'ogc_fid'); 
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_nfl', 'sk01_l3_to_sk_utm_l1_map');
 
 ------------------------
 -- GEO
 ------------------------
-SELECT TT_Prepare('translation', 'sk_utm01_geo', '_sk_geo', 'ab_avi01_geo'); 
+SELECT TT_Prepare('translation', 'sk_utm01_geo', '_sk01_geo', 'ab_avi01_geo'); 
 
 SELECT TT_CreateMappingView('rawfri', 'sk01', 1, 'sk_utm', 1);
 
@@ -126,7 +126,7 @@ DELETE FROM casfri50.geo_all WHERE left(cas_id, 4) = 'SK01';
 
 -- Add translated ones
 INSERT INTO casfri50.geo_all -- 
-SELECT * FROM TT_Translate_sk_geo('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
+SELECT * FROM TT_Translate_sk01_geo('rawfri', 'sk01_l1_to_sk_utm_l1_map', 'ogc_fid');
 
 SELECT * FROM TT_ShowLastLog('translation', 'sk_utm01_geo', 'sk01_l1_to_sk_utm_l1_map');
 --------------------------------------------------------------------------
