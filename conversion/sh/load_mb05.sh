94418c06e6544863f3ac0b6b5c585d6fbed80c4f
Marc <medwards219@gmail.com>
2020-07-23 18:15:08 -0400

    #231 - MB05 and 06 should add poly_id to the source data on loading.

--- conversion/sh/load_mb05.sh
+++ conversion/sh/load_mb05.sh
@@ -14,6 +14,10 @@
 # If the table already exists, it can be overwritten by setting the "overwriteFRI" variable 
 # in the configuration file.
 
+# Source data does not have a unique set of identifiers that can trace from the cas_id 
+# back to the source polygon.
+# We will add a unique ID (poly_id) to the table before loading.
+
 ######################################## Set variables #######################################
 
 source ./common.sh
@@ -22,13 +26,29 @@ inventoryID=MB05
 
 srcFileName=MFAGeodatabase
 srcFullPath="$friDir/MB/$inventoryID/data/inventory/$srcFileName.gdb"
+MB_subFolder=MB/$inventoryID/data/inventory/
+
 gdbTableName=MB_FRIFLI_Updatedto2010FINAL_v6
 fullTargetTableName=$targetFRISchema.mb05
 
 ########################################## Process ######################################
 
-# Run ogr2ogr to load all table
+# Standard SQL code used to add and drop columns in gdbs. If column is not present the DROP command
+# will return an error which can be ignored.
+# SQLite is needed to add the id based on rowid.
+# Should be activated only at the first load otherwise it would brake the translation tables tests. 
+# Only runs once, when flag file poly_id_added.txt does not exist.
 
+if [ ! -e "$friDir/$MB_subFolder/poly_id_added.txt" ]; then
+
+	"$gdalFolder/ogrinfo" $srcFullPath $gdbTableName -sql "ALTER TABLE $gdbTableName DROP COLUMN poly_id"
+	"$gdalFolder/ogrinfo" $srcFullPath $gdbTableName -sql "ALTER TABLE $gdbTableName ADD COLUMN poly_id integer"
+	"$gdalFolder/ogrinfo" $srcFullPath $gdbTableName -dialect SQLite -sql "UPDATE $gdbTableName set poly_id = rowid"
+
+	echo " " > "$friDir/$MB_subFolder/poly_id_added.txt"
+fi
+
+# Run ogr2ogr to load all table
 "$gdalFolder/ogr2ogr" \
 -f "PostgreSQL" "$pg_connection_string" "$srcFullPath" "$gdbTableName" \
 -nln $fullTargetTableName $layer_creation_options $other_options \
@@ -38,4 +58,3 @@ fullTargetTableName=$targetFRISchema.mb05
 createSQLSpatialIndex=True
 
 source ./common_postprocessing.sh
-
