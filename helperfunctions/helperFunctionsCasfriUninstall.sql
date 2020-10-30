94ca245fe172810e26cc8968a1b0f120cedb18fc
Marc <medwards219@gmail.com>
2020-10-30 11:58:03 -0400

    Changed qc stand structure lookup and origin function to use generic names

--- helperfunctions/helperFunctionsCasfriUninstall.sql
+++ helperfunctions/helperFunctionsCasfriUninstall.sql
@@ -100,7 +100,7 @@ DROP FUNCTION IF EXISTS TT_nl_nli01_productivity_translation(text, text);
 DROP FUNCTION IF EXISTS TT_nl_nli01_productivity_type_translation(text, text);
 DROP FUNCTION IF EXISTS TT_nl_nli01_origin_upper_translation(text, text, text);
 DROP FUNCTION IF EXISTS TT_nl_nli01_origin_lower_translation(text, text, text);
-DROP FUNCTION IF EXISTS TT_qc_ini03_origin_translation(text, text);
+DROP FUNCTION IF EXISTS TT_qc_origin_translation(text, text);
 
 -------------------------------------------------------------------------------
 -- Generic functions
