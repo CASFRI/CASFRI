28e72195071964a19addc3d65e3c2b3f4e0d0fb0
Pierre <pierre.racine@sbf.ulaval.ca>
2020-11-18 12:05:22 -0500

    Updated counts in translation_test

--- translation/test/testTranslation.sql
+++ translation/test/testTranslation.sql
@@ -152,7 +152,7 @@ ORDER BY cas_id, inventory_id, orig_stand_id, stand_structure,
          num_of_layers, map_sheet_id, casfri_area, 
          casfri_perimeter, src_inv_area, stand_photo_year; 
 ------------------------
-SELECT count(*) FROM casfri50_test.cas_all_new; -- 7100
+SELECT count(*) FROM casfri50_test.cas_all_new; -- 11600
 -------------------------------------------------------
 -- Translate all DST tables into a common table
 -------------------------------------------------------
@@ -269,7 +269,7 @@ ORDER BY cas_id, layer,
          dist_type_2, dist_year_2, dist_ext_upper_2, dist_ext_lower_2,
          dist_type_3, dist_year_3, dist_ext_upper_3, dist_ext_lower_3;
 ------------------------
-SELECT count(*) FROM casfri50_test.dst_all_new; -- 7860
+SELECT count(*) FROM casfri50_test.dst_all_new; -- 13262
 -------------------------------------------------------
 -- Translate all ECO tables into a common table
 -------------------------------------------------------
@@ -592,7 +592,7 @@ ORDER BY cas_id, layer, soil_moist_reg, structure_per, layer, layer_rank, crown_
          species_6, species_per_6, species_7, species_per_7, species_8, species_per_8, species_9, species_per_9, species_10, species_per_10, 
          origin_upper, origin_lower, site_class, site_index;
 ------------------------
-SELECT count(*) FROM casfri50_test.lyr_all_new; -- 12852
+SELECT count(*) FROM casfri50_test.lyr_all_new; -- 23129
 -------------------------------------------------------
 -- Translate all NFL tables into a common table
 -------------------------------------------------------
@@ -775,7 +775,7 @@ SELECT * FROM casfri50_test.nfl_all_new
 ORDER BY cas_id, layer, layer_rank, soil_moist_reg, structure_per, crown_closure_upper, crown_closure_lower, 
          height_upper, height_lower, nat_non_veg, non_for_anth, non_for_veg;
 ------------------------
-SELECT count(*) FROM casfri50_test.nfl_all_new; -- 10838
+SELECT count(*) FROM casfri50_test.nfl_all_new; -- 20439
 ---------------------------------------------------------
 
 ---------------------------------------------------------
