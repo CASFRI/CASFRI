1710cf7989ae06e7331d200c0c912db08a9fb7ec
Pierre <pierre.racine@sbf.ulaval.ca>
2020-12-15 16:30:45 -0500

    Closes #484. script able to launch all other loading scripts

---
+++ conversion/sh/load_all.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+"/c/program files/git/git-bash.exe" load_ab_photoYear.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_ab06.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_ab16.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_bc08.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_bc10.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_mb05.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_mb06.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_nb01.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_nb02.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_nl_photoyear.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_nl01.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_ns03.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_nt01.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_nt02.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_on02.bat
+
+"/c/program files/git/git-bash.exe" load_pc02.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_pe01.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_qc01.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_qc02.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_qc03.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_qc04.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_qc05.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_sk01.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_sk02.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_sk03.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_sk04.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_sk05.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_sk06.sh & > /dev/null
+"/c/program files/git/git-bash.exe" load_yt02.sh & > /dev/null
