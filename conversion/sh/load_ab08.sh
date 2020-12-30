28ad28f03a3448cdeb96d5da2b87e3c8c20f444c
Marc <medwards219@gmail.com>
2020-12-30 12:28:29 -0500

    #290 New AB loading scripts

---
+++ conversion/sh/load_ab08.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+# This script loads the AB GOV MILLAR WESTIN dataset
+
+# The format of the source dataset is a single shapefile named millar.shp.
+# poly_num is a unique id.
+
+# The year of photography is included as a shapefile. Photo year will be joined to the 
+# loaded table in PostgreSQL
+
+# Load into a target table in the schema defined in the config file.
+
+# If the table already exists, it can be overwritten by setting the "overwriteFRI" variable 
+# in the configuration file.
+
+######################################## Set variables #######################################
+
+source ./common.sh
+
+inventoryID=AB08
+
+srcFileName=millar
+srcFullPath="$friDir/AB/$inventoryID/data/inventory/$srcFileName.shp"
+
+fullTargetTableName=$targetFRISchema.ab08
+
+########################################## Process ######################################
+
+"$gdalFolder/ogr2ogr" \
+-f PostgreSQL "$pg_connection_string" "$srcFullPath" \
+-nln $fullTargetTableName $layer_creation_options $other_options \
+-nlt PROMOTE_TO_MULTI \
+-sql "SELECT *, '$srcFileName' AS src_filename, '$inventoryID' AS inventory_id FROM '$srcFileName'" \
+-progress $overwrite_tab
+
+source ./common_postprocessing.sh
