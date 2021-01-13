5893d0dc95063cbf40e0785eb57687d6868a5cf1
Melina Houle <houle.melina@gmail.com>
2021-01-13 08:50:55 -0500

    #509 - PC01 Update and document translation tables

--- workflow/01_develTranslationTables/PC01.sql
+++ workflow/01_develTranslationTables/PC01.sql
@@ -35,27 +35,15 @@ SELECT TT_CreateMappingView('rawfri', 'pc01', 'pc_panp', 200);
 SELECT * FROM TT_Translate_pc01_cas_devel('rawfri', 'pc01_l1_to_pc_panp_l1_map_200', 'ogc_fid');
 SELECT * FROM TT_ShowLastLog('translation_devel', 'pc_panp01_cas_devel');
 
--- DST ATTRIBUTES
-SELECT * FROM translation.pc_panp01_dst;
-DROP TABLE IF EXISTS translation_devel.pc01_panp01_dst_devel;
-CREATE TABLE translation_devel.pc01_panp01_dst_devel AS
-SELECT * FROM translation.pc_panp01_dst; --WHERE rule_id::int = 1
-SELECT * FROM translation_devel.pc01_panp01_dst_devel;
-SELECT TT_Prepare('translation_devel', 'pc01_panp01_dst_devel', '_pc01_dst_devel');
-SELECT TT_CreateMappingView('rawfri', 'pc01', 1, 'pc_panp', 1, 200);
-SELECT * FROM TT_Translate_pc01_dst_devel('rawfri', 'pc01_l1_to_pc_panp_l1_map_200', 'ogc_fid'); -- 4 s.
-SELECT * FROM TT_ShowLastLog('translation_devel', 'pc_panp01_dst_devel');
-
-SELECT a.cas_id, b.dist_type_1, a.dist_type_1, a.dist_type_2 
-FROM TT_Translate_pc01_dst_devel('rawfri', 'pc01_l1_to_pc_panp_l1_map_200') a, rawfri.pc01_l1_to_pc_panp_l1_map_200 b
-WHERE b.ogc_fid::int = right(a.cas_id, 7)::int;
+-- DST ATTRIBUTES 
+-- No disturbance history found in PC01
 
 -- LYR1 ATTRIBUTES
 SELECT * FROM translation.pc_panp01_lyr;
 DROP TABLE IF EXISTS translation_devel.pc01_panp01_lyr_devel;
 CREATE TABLE translation_devel.pc01_panp01_lyr_devel AS
-SELECT * FROM translation.pc_panp01_lyr 
-WHERE rule_id::int < 6;
+SELECT * FROM translation.pc_panp01_lyr;
+--WHERE rule_id::int < 6;
 SELECT * FROM translation_devel.pc01_panp01_lyr_devel;
 SELECT TT_Prepare('translation_devel', 'pc01_panp01_lyr_devel', '_pc01_lyr_devel');
 SELECT TT_CreateMappingView('rawfri', 'pc01', 1, 'pc_panp', 1, 200);
@@ -73,20 +61,20 @@ SELECT * FROM TT_ShowLastLog('translation_devel', 'pc01_panp01_lyr_devel');
 
 -- NFL ATTRIBUTES
 SELECT * FROM translation.pc_panp01_nfl;
-DROP TABLE IF EXISTS translation_devel.pc01_panp01_nfl_devel;
+DROP TABLE IF EXISTS translation_devel.pc01_panp01_nfl_devel; 
 CREATE TABLE translation_devel.pc01_panp01_nfl_devel AS
 SELECT * FROM translation.pc_panp01_nfl; --WHERE rule_id::int != 4; --IN (0,1,2,3);
 SELECT * FROM translation_devel.pc01_panp01_nfl_devel;
-SELECT TT_Prepare('translation_devel', 'pc01_panp01_nfl_devel', '_pc01_nfl_devel');
 
-SELECT TT_CreateMappingView('rawfri', 'pc01', 4, 'pc_panp', 1, 200);
-SELECT * FROM TT_Translate_pc01_nfl_devel('rawfri', 'pc01_l4_to_pc_panp_l1_map_200', 'ogc_fid');
+SELECT TT_Prepare('translation_devel', 'pc01_panp01_nfl_devel', '_pc01_nfl_devel');
+SELECT TT_CreateMappingView('rawfri', 'pc01', 4, 'pc_panp', 1, 8094);
+SELECT * FROM TT_Translate_pc01_nfl_devel('rawfri', 'pc01_l4_to_pc_panp_l1_map_8094', 'ogc_fid');
 SELECT * FROM TT_ShowLastLog('translation_devel', 'pc01_panp01_nfl_devel');
 
 SELECT TT_CreateMappingView('rawfri', 'pc01', 5, 'pc_panp', 1, 200);
 SELECT * FROM TT_Translate_pc01_nfl_devel('rawfri', 'pc01_l5_to_pc_panp_l1_map_200', 'ogc_fid');
 
-SELECT TT_CreateMappingView('rawfri', 'pc01', 6, 'pc_panp', 1, 200);
+SELECT TT_CreateMappingView('rawfri', 'pc01', 6, 'pc_panp', 1, 200); 
 SELECT * FROM TT_Translate_pc01_nfl_devel('rawfri', 'pc01_l6_to_pc_panp_l1_map_200', 'ogc_fid');
 
 -- ECO ATTRIBUTES
