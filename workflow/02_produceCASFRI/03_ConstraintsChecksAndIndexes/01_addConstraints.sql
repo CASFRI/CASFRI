5ad765421475a603bcc544322b78f6cdbb16a600
Pierre <pierre.racine@sbf.ulaval.ca>
2021-02-18 16:39:57 -0500

    #614 Better link to issue

--- workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/01_addConstraints.sql
+++ workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/01_addConstraints.sql
@@ -1132,7 +1132,7 @@ FROM (SELECT *
 UNION ALL
 SELECT '4.46'::text number,
        'lyr_all' target_table,
-       'Issue #614. Wrong error code -8886. Ensure LYR table HEIGHT_LOWER is smaller than or equal to HEIGHT_UPPER' description, 
+       'Ensure LYR table HEIGHT_LOWER is smaller than or equal to HEIGHT_UPPER' description, 
        passed, cstr_query
 FROM (SELECT * 
       FROM TT_AddConstraint('casfri50', 'lyr_all', 'CHECK', 
@@ -1157,7 +1157,7 @@ FROM (SELECT *
 UNION ALL
 SELECT '4.48'::text number,
        'lyr_all' target_table,
-       'Ensure LYR table SPECIES_1 values match the corresponding lookup table' description, 
+       'Issue #614. Wrong error code -8886. Ensure LYR table SPECIES_1 values match the corresponding lookup table' description, 
        passed, cstr_query
 FROM (SELECT * 
       FROM TT_AddConstraint('casfri50', 'lyr_all', 'FK', 
@@ -1166,7 +1166,7 @@ FROM (SELECT *
 UNION ALL
 SELECT '4.49'::text number,
        'lyr_all' target_table,
-       'Ensure LYR table SPECIES_2 values match the corresponding lookup table' description, 
+       'Issue #614. Wrong error code -8886. Ensure LYR table SPECIES_2 values match the corresponding lookup table' description, 
        passed, cstr_query
 FROM (SELECT * 
       FROM TT_AddConstraint('casfri50', 'lyr_all', 'FK', 
@@ -1175,7 +1175,7 @@ FROM (SELECT *
 UNION ALL
 SELECT '4.50'::text number,
        'lyr_all' target_table,
-       'Ensure LYR table SPECIES_3 values match the corresponding lookup table' description, 
+       'Issue #614. Wrong error code -8886. Ensure LYR table SPECIES_3 values match the corresponding lookup table' description, 
        passed, cstr_query
 FROM (SELECT * 
       FROM TT_AddConstraint('casfri50', 'lyr_all', 'FK', 
