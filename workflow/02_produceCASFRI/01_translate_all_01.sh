f0c608717b74483487c7beff9b6da2c5188f0b39
Pierre <pierre.racine@sbf.ulaval.ca>
2021-02-23 17:18:20 -0500

    #605 Added QC02, QC06, NS01 and NS02 and reordered inventories in alpha order

--- workflow/02_produceCASFRI/01_translate_all_01.sh
+++ workflow/02_produceCASFRI/01_translate_all_01.sh
@@ -6,10 +6,12 @@ source ../../config.sh
 # BC08 4677411
 # BC10 5151772
 # ON02 3629073
+# QC02 2876326
 # QC04 2487519
 # QC05 6768074
+# QC06 4809274
 
-for F in BC08 BC10 ON02 QC04 QC05
+for F in BC08 BC10 ON02 QC02 QC04 QC05 QC06
 do
   "/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./02_perInventory/02_$F.sql" &
 done
--- workflow/02_produceCASFRI/01_translate_all_02.sh
+++ workflow/02_produceCASFRI/01_translate_all_02.sh
@@ -9,13 +9,13 @@ source ../../config.sh
 # NB01  927177
 # NB02 1123893
 # NL01 1863664
+# NS01 1127926
+# NS02 1090671
 # NS03  995886
 # SK01 1501667
 # SK04  633522
-# NS01 1127926
-# NS02 1090671
 
-for F in AB25 AB29 MB05 NB01 NB02 NL01 NS03 SK01 SK04 NS01 NS02
+for F in AB25 AB29 MB05 NB01 NB02 NL01 NS01 NS02 NS03 SK01 SK04
 do
   "/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./02_perInventory/02_$F.sql" &
 done
