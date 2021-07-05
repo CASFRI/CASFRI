2826202ec58d996c7807ccb2d1f418e70b0c592f
Pierre <pierre.racine@sbf.ulaval.ca>
2021-07-05 14:57:44 -0400

    #733 DROP indexes if they exists before creating them

--- workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/02_addIndexes.sql
+++ workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/02_addIndexes.sql
@@ -12,73 +12,93 @@
 --                         Pierre Vernier <pierre.vernier@gmail.com>
 -------------------------------------------------------------------------------
 -- Add indexes to CAS_ALL
+DROP INDEX IF EXISTS cas_all_casid_idx;
 CREATE INDEX cas_all_casid_idx
 ON casfri50.cas_all USING btree(cas_id);
 
+DROP INDEX IF EXISTS cas_all_inventoryid_idx;
 CREATE INDEX cas_all_inventoryid_idx
 ON casfri50.cas_all USING btree(inventory_id);
 
+DROP INDEX IF EXISTS cas_all_casidinv_idx;
 CREATE INDEX cas_all_casidinv_idx
 ON casfri50.cas_all USING btree(left(cas_id, 4));
     
+DROP INDEX IF EXISTS cas_all_province_idx;
 CREATE INDEX cas_all_province_idx
 ON casfri50.cas_all USING btree(left(cas_id, 2));
 
 --------------------------------------------------------------------------
 -- Add indexes to DST_ALL
+DROP INDEX IF EXISTS dst_all_casid_idx;
 CREATE INDEX dst_all_casid_idx
 ON casfri50.dst_all USING btree(cas_id);
 
+DROP INDEX IF EXISTS dst_all_inventory_idx;
 CREATE INDEX dst_all_inventory_idx
 ON casfri50.dst_all USING btree(left(cas_id, 4));
     
+DROP INDEX IF EXISTS dst_all_province_idx;
 CREATE INDEX dst_all_province_idx
 ON casfri50.dst_all USING btree(left(cas_id, 2));
 
 --------------------------------------------------------------------------
 -- Add indexes to ECO_ALL
+DROP INDEX IF EXISTS eco_all_casid_idx;
 CREATE INDEX eco_all_casid_idx
 ON casfri50.eco_all USING btree(cas_id);
 
+DROP INDEX IF EXISTS eco_all_inventory_idx;
 CREATE INDEX eco_all_inventory_idx
 ON casfri50.eco_all USING btree(left(cas_id, 4));
     
+DROP INDEX IF EXISTS eco_all_province_idx;
 CREATE INDEX eco_all_province_idx
 ON casfri50.eco_all USING btree(left(cas_id, 2));
 
 --------------------------------------------------------------------------
 -- Add indexes to LYR_ALL
+DROP INDEX IF EXISTS lyr_all_casid_idx;
 CREATE INDEX lyr_all_casid_idx
 ON casfri50.lyr_all USING btree(cas_id);
 
+DROP INDEX IF EXISTS lyr_all_inventory_idx;
 CREATE INDEX lyr_all_inventory_idx
 ON casfri50.lyr_all USING btree(left(cas_id, 4));
     
+DROP INDEX IF EXISTS lyr_all_province_idx;
 CREATE INDEX lyr_all_province_idx
 ON casfri50.lyr_all USING btree(left(cas_id, 2));
 
 --------------------------------------------------------------------------
 -- Add indexes to NFL_ALL
+DROP INDEX IF EXISTS nfl_all_casid_idx;
 CREATE INDEX nfl_all_casid_idx
 ON casfri50.nfl_all USING btree(cas_id);
 
+DROP INDEX IF EXISTS nfl_all_inventory_idx;
 CREATE INDEX nfl_all_inventory_idx
 ON casfri50.nfl_all USING btree(left(cas_id, 4));
     
+DROP INDEX IF EXISTS nfl_all_province_idx;
 CREATE INDEX nfl_all_province_idx
 ON casfri50.nfl_all USING btree(left(cas_id, 2));
 
 --------------------------------------------------------------------------
 -- Add indexes to GEO_ALL
+DROP INDEX IF EXISTS geo_all_casid_idx;
 CREATE INDEX geo_all_casid_idx
 ON casfri50.geo_all USING btree(cas_id);
 
+DROP INDEX IF EXISTS geo_all_inventory_idx;
 CREATE INDEX geo_all_inventory_idx
 ON casfri50.geo_all USING btree(left(cas_id, 4));
     
+DROP INDEX IF EXISTS geo_all_province_idx;
 CREATE INDEX geo_all_province_idx
 ON casfri50.geo_all USING btree(left(cas_id, 2));
 
+DROP INDEX IF EXISTS geo_all_geom_idx;
 CREATE INDEX geo_all_geom_idx
 ON casfri50.geo_all USING gist(geometry);
 --------------------------------------------------------------------------
