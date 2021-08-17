eb406eade97698a2580c40c224253233ef73f5ba
Pierre <pierre.racine@sbf.ulaval.ca>
2021-08-17 11:18:29 -0400

    #733 Closes #755 Updated all headers

--- workflow/02_produceCASFRI/02_perInventory/02_MB02.sql
+++ workflow/02_produceCASFRI/02_perInventory/02_MB02.sql
@@ -1,15 +1,16 @@
 ------------------------------------------------------------------------------
--- CASFRI Sample workflow file for CASFRI v5 beta
--- For use with PostgreSQL Table Tranlation Engine v0.1 for PostgreSQL 9.x
--- https://github.com/edwardsmarc/postTranslationEngine
--- https://github.com/edwardsmarc/casfri
+-- CASFRI - MB02 translation script for CASFRI v5
+-- For use with PostgreSQL Table Tranlation Framework v2.0.1 for PostgreSQL 13.x
+-- https://github.com/CASFRI/PostgreSQL-Table-Translation-Framework
+-- https://github.com/CASFRI/CASFRI
 --
 -- This is free software; you can redistribute and/or modify it under
 -- the terms of the GNU General Public Licence. See the COPYING file.
 --
--- Copyright (C) 2018-2020 Pierre Racine <pierre.racine@sbf.ulaval.ca>, 
+-- Copyright (C) 2018-2021 Pierre Racine <pierre.racine@sbf.ulaval.ca>, 
 --                         Marc Edwards <medwards219@gmail.com>,
 --                         Pierre Vernier <pierre.vernier@gmail.com>
+--                         Melina Houle <melina.houle@sbf.ulaval.ca>
 -------------------------------------------------------------------------------
 -- No not display debug messages.
 SET tt.debug TO TRUE;
