596bddf509a5e67b6cbee96c4cd179bc924ab6b9
Melina Houle <houle.melina@gmail.com>
2022-02-17 23:05:50 -0500

    #768 - Drop poly_num = 0 during the upload

--- conversion/sh/load_ab32.sh
+++ conversion/sh/load_ab32.sh
@@ -2,11 +2,12 @@
 
 # This script loads the Alpac updated avi data (savi 2001-2011) into PostgreSQL
 
-# The format of the source dataset is a geodtabase divided into mapsheets.
+# The format of the source dataset is a geodtabase.
 
 # The year of photography is included as a shapefile. Photo year will be joined to the 
 # loaded table in PostgreSQL. 
-# Photo year will be joined to the loaded table in PostgreSQL
+# Drop polygons where poly_num = 0, these have no data.
+# Once poly_num = 0 are removed, poly_num is unique. 
 
 # Load into the ab32 target table in the schema defined in the config file.
 
@@ -32,14 +33,6 @@ overwrite_option="$overwrite_tab"
 
 
 ########################################## Process ######################################
-
-# Loop through all mapsheets.
-# For first load, set -lco PRECISION=NO to avoid type errors on import. Remove for following loads.
-# Set -overwrite for first load if requested in config
-# After first load, remove -overwrite and add -update -append
-# Two fields (FOREST# and FOREST-ID) don't load correctly because field names are not valid in PostgreSQL. Create two new columns (forest_id_1 and forest_id_2) with valid field names to hold these variables.
-# Original columns will be loaded as forest_ and forest_id, they will be NULL because ogr2ogr cannot append the values from the invalid field names.
-# New fields will be added to the right of the table
 
 # Run ogr2ogr to load FRIs
 "$gdalFolder/ogr2ogr" \
