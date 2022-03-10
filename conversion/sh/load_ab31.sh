73bbe9eba0b7b16cd6b456b80d3a6503e50962da
Melina Houle <houle.melina@gmail.com>
2022-03-09 23:03:25 -0500

    #767 - Update AB31 loading script ant inventory_list

--- conversion/sh/load_ab31.sh
+++ conversion/sh/load_ab31.sh
@@ -1,12 +1,15 @@
 #!/bin/bash -x
 
-# This script loads the Alpac avi data (AB31) into PostgreSQL
+# This script loads the Alpac avi data (AB31) into PostgreSQL.
 
-# The format of the source dataset is a geodtabase divided into mapsheets.
+# The format of the source dataset is two geodatabases. Poly_id is the unique identifier in table Approved_avi_nad83, while 
+# POLY_NUM is the unique identifier in table AVI_nad27_missing_maps. Poly_id is missing in AVI_nad27_missing_maps. During 
+# the upload, we use poly_num from AVI_nad27_missing_maps to fill poly_id, which will become the unique identifier.
+
+# Only polygons having inventory attributes (MER >0) are loaded. 
 
 # The year of photography is included as a shapefile. Photo year will be joined to the 
 # loaded table in PostgreSQL. 
-# poly_id is the unique identifier.
 
 # Load into the ab31 target table in the schema defined in the config file.
 
@@ -18,9 +21,13 @@
 source ./common.sh
 
 inventoryID=AB31
-srcFileName=Approved_avi_nad83
-gdbTableName=avi_poly_nad83_csrs
-srcFullPath=$friDir/AB/$inventoryID/data/inventory/$srcFileName.gdb
+srcFileNameMain=Approved_avi_nad83
+gdbTableNameMain=avi_poly_nad83_csrs
+srcFullPathMain=$friDir/AB/$inventoryID/data/inventory/$srcFileNameMain.gdb
+
+srcFileNameMissing=AVI_nad27_missing_maps
+gdbTableNameMissing=AVI_nad27_missing_maps
+srcFullPathMissing=$friDir/AB/$inventoryID/data/inventory/$srcFileNameMissing.gdb
 
 fullTargetTableName=$targetFRISchema.ab31
 
@@ -41,12 +48,23 @@ overwrite_option="$overwrite_tab"
 # Original columns will be loaded as forest_ and forest_id, they will be NULL because ogr2ogr cannot append the values from the invalid field names.
 # New fields will be added to the right of the table
 
+
 # Run ogr2ogr to load FRIs
 "$gdalFolder/ogr2ogr" \
--f "PostgreSQL" "$pg_connection_string" "$srcFullPath" "$gdbTableName" \
+-f PostgreSQL "$pg_connection_string" "$srcFullPathMain" \
 -nln $fullTargetTableName $layer_creation_options $other_options \
--sql "SELECT *, '$srcFileName' AS src_filename, '$inventoryID' AS inventory_id FROM $gdbTableName" \
+-nlt PROMOTE_TO_MULTI \
+-sql "SELECT *, '$srcFileNameMain' AS src_filename, '$inventoryID' AS inventory_id FROM $gdbTableNameMain WHERE MER <> 0" \
 -progress $overwrite_tab
+
+# Load missing part
+"$gdalFolder/ogr2ogr" \
+-update -append -addfields \
+-f PostgreSQL "$pg_connection_string" "$srcFullPathMissing" \
+-nln $fullTargetTableName $other_options \
+-nlt PROMOTE_TO_MULTI \
+-sql "SELECT *, '$srcFileNameMissing' AS src_filename, '$inventoryID' AS inventory_id, '$poly_num' AS poly_id FROM $gdbTableNameMissing WHERE MER <> 0" \
+-progress
 
 
 # Run ogr2ogr to load Alpac photoyear
