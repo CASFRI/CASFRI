4c62609983131ce53aba2339352dcb8cc2d4a179
Melina Houle <houle.melina@gmail.com>
2024-01-22 11:38:04 -0500

    #785 - Load BC04

---
+++ conversion/sh/load_bc04.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+
+# This script loads the British Columbia VRI forest inventory BC04 into PostgreSQL
+
+# The format of the source dataset is a geodatabase
+
+# The year of photography is included in the attributes table (REFERENCE_YEAR)
+
+# Load into a target table in the schema defined in the config file.
+
+# If the table already exists, it can be overwritten by setting the "overwriteFRI" variable 
+# in the configuration file.
+
+######################################## Set variables #######################################
+
+source ./common.sh
+
+inventoryID=BC04
+srcFileName=VEG_COMP_LYR_R1_POLY
+gdbFileName=VEG_COMP_LYR_R1_POLY
+srcFullPath="$friDir/BC/$inventoryID/data/inventory/$srcFileName.gdb"
+
+fullTargetTableName=$targetFRISchema.bc04
+
+########################################## Process ######################################
+
+# Run ogr2ogr
+"$gdalFolder/ogr2ogr" \
+-f PostgreSQL "$pg_connection_string" "$srcFullPath" \
+-nln $fullTargetTableName $layer_creation_options $other_options \
+-sql "SELECT *, '$srcFileName' AS src_filename, '$inventoryID' AS inventory_id FROM $gdbFileName" \
+-progress $overwrite_tab
+
+source ./common_postprocessing.sh
+
+
