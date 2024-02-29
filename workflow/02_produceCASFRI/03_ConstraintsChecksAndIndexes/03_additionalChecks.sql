f35a60d514926760eff141b0e5b965e393451a9c
pedrogit <pierre.racine@sbf.ulaval.ca>
2024-02-29 08:42:53 -0500

    Minor updates to flat table scripts

--- workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/03_additionalChecks.sql
+++ workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/03_additionalChecks.sql
@@ -29,19 +29,18 @@ WHERE NOT ((stand_structure = ''SINGLE_LAYERED'' AND nb_lyrlayers = 1) OR
            (stand_structure != ''SINGLE_LAYERED'' AND stand_structure != ''MULTI_LAYERED'' AND stand_structure != ''COMPLEX''));
 ' list_query
 FROM (
-WITH lyr_structure AS (
-  SELECT DISTINCT stand_structure, count(*) nb_lyrlayers
-  FROM casfri50.cas_all cas 
-  LEFT JOIN casfri50.lyr_all lyr USING (cas_id)
-  GROUP BY cas.cas_id
-)
-SELECT count(*) = 0 passed
-FROM lyr_structure
-WHERE NOT ((stand_structure = 'SINGLE_LAYERED' AND nb_lyrlayers = 1) OR
-           (stand_structure = 'COMPLEX' AND (nb_lyrlayers = 1 OR nb_lyrlayers = 2)) OR
-           (stand_structure = 'MULTI_LAYERED' AND nb_lyrlayers > 1) OR 
-           (stand_structure != 'SINGLE_LAYERED' AND stand_structure != 'MULTI_LAYERED' AND stand_structure != 'COMPLEX'))
-           
+  WITH lyr_structure AS (
+    SELECT DISTINCT stand_structure, count(*) nb_lyrlayers
+    FROM casfri50.cas_all cas 
+    LEFT JOIN casfri50.lyr_all lyr USING (cas_id)
+    GROUP BY cas.cas_id
+  )
+  SELECT count(*) = 0 passed
+  FROM lyr_structure
+  WHERE NOT ((stand_structure = 'SINGLE_LAYERED' AND nb_lyrlayers = 1) OR
+             (stand_structure = 'COMPLEX' AND (nb_lyrlayers = 1 OR nb_lyrlayers = 2)) OR
+             (stand_structure = 'MULTI_LAYERED' AND nb_lyrlayers > 1) OR 
+             (stand_structure != 'SINGLE_LAYERED' AND stand_structure != 'MULTI_LAYERED' AND stand_structure != 'COMPLEX'))
 ) foo
 -------------------------------------------------------
 UNION ALL
@@ -61,16 +60,18 @@ WHERE lyr.cas_id IS NULL AND
       eco.cas_id IS NULL
 GROUP BY inventory_id;
 ' list_query
-FROM (SELECT count(*) = 0 passed
-      FROM casfri50.cas_all cas
-      LEFT JOIN casfri50.lyr_all lyr USING (cas_id)
-      LEFT JOIN casfri50.nfl_all nfl USING (cas_id)
-      LEFT JOIN casfri50.dst_all dst USING (cas_id)
-      LEFT JOIN casfri50.eco_all eco USING (cas_id)
-      WHERE lyr.cas_id IS NULL AND
-            nfl.cas_id IS NULL AND
-            dst.cas_id IS NULL AND
-            eco.cas_id IS NULL) foo
+FROM (
+  SELECT count(*) = 0 passed
+  FROM casfri50.cas_all cas
+  LEFT JOIN casfri50.lyr_all lyr USING (cas_id)
+  LEFT JOIN casfri50.nfl_all nfl USING (cas_id)
+  LEFT JOIN casfri50.dst_all dst USING (cas_id)
+  LEFT JOIN casfri50.eco_all eco USING (cas_id)
+  WHERE lyr.cas_id IS NULL AND
+        nfl.cas_id IS NULL AND
+        dst.cas_id IS NULL AND
+        eco.cas_id IS NULL
+) foo
 -------------------------------------------------------
 UNION ALL
 SELECT '1.3'::text number,
@@ -91,19 +92,19 @@ NATURAL LEFT JOIN all_layers_counts a
 WHERE NOT ((c.num_of_layers = -8886 AND a.cnt IS NULL) OR (c.num_of_layers = a.cnt))
 ORDER BY c.num_of_layers, a.cnt;' list_query
 FROM (
-WITH all_layers AS (
-  SELECT cas_id FROM casfri50.lyr_all
-  UNION ALL
-  SELECT cas_id FROM casfri50.nfl_all
-), all_layers_counts AS (
-  SELECT cas_id, count(*) cnt
-  FROM all_layers
-  GROUP BY cas_id
-)
-SELECT count(*) = 0 passed
-FROM casfri50.cas_all c
-NATURAL LEFT JOIN all_layers_counts a 
-WHERE NOT ((c.num_of_layers = -8886 AND a.cnt IS NULL) OR (c.num_of_layers = a.cnt))
+  WITH all_layers AS (
+    SELECT cas_id FROM casfri50.lyr_all
+    UNION ALL
+    SELECT cas_id FROM casfri50.nfl_all
+  ), all_layers_counts AS (
+    SELECT cas_id, count(*) cnt
+    FROM all_layers
+    GROUP BY cas_id
+  )
+  SELECT count(*) = 0 passed
+  FROM casfri50.cas_all c
+  NATURAL LEFT JOIN all_layers_counts a 
+  WHERE NOT ((c.num_of_layers = -8886 AND a.cnt IS NULL) OR (c.num_of_layers = a.cnt))
 ) foo
 -------------------------------------------------------
 UNION ALL
@@ -147,41 +148,41 @@ WHERE layers_order != ''lyr1'' AND
       layers_order != ''nfl1_nfl2_nfl3_nfl4'';
 ' list_query
 FROM (
-WITH all_layers_numbers AS (
-  SELECT cas_id, 'lyr' || layer::text layer  FROM casfri50.lyr_all
-  UNION ALL
-  SELECT cas_id, 'nfl' || layer::text layer  FROM casfri50.nfl_all
-), all_layers_orders AS (
-  SELECT cas_id, string_agg(layer::text, '_' ORDER BY layer) layers_order
-  FROM all_layers_numbers
-  GROUP BY cas_id
-)
-SELECT count(*) = 0 passed
-FROM all_layers_orders
-WHERE layers_order != 'lyr1' AND
-      layers_order != 'lyr1_lyr2' AND
-      layers_order != 'lyr1_lyr2_lyr3' AND
-      layers_order != 'lyr1_lyr2_lyr3_lyr4' AND
-      layers_order != 'lyr1_lyr2_lyr3_lyr4_lyr5' AND
-      layers_order != 'lyr1_lyr2_lyr3_lyr4_nfl5' AND
-      layers_order != 'lyr1_lyr2_lyr3_lyr4_nfl5_nfl6' AND
-      layers_order != 'lyr1_lyr2_lyr3_lyr4_nfl5_nfl6_nfl7' AND
-      layers_order != 'lyr1_lyr2_lyr3_nfl4' AND
-      layers_order != 'lyr1_lyr2_lyr3_nfl4_nfl5' AND
-      layers_order != 'lyr1_lyr2_lyr3_nfl4_nfl5_nfl6' AND
-      layers_order != 'lyr1_lyr2_lyr3_nfl4_nfl5_nfl6_nfl7' AND
-      layers_order != 'lyr1_lyr2_nfl3' AND
-      layers_order != 'lyr1_lyr2_nfl3_nfl4' AND
-      layers_order != 'lyr1_lyr2_nfl3_nfl4_nfl5' AND
-      layers_order != 'lyr1_lyr2_nfl3_nfl4_nfl5_nfl6' AND
-      layers_order != 'lyr1_nfl2' AND
-      layers_order != 'lyr1_nfl2_nfl3' AND
-      layers_order != 'lyr1_nfl2_nfl3_nfl4' AND
-      layers_order != 'lyr1_nfl2_nfl3_nfl4_nfl5' AND
-      layers_order != 'nfl1' AND
-      layers_order != 'nfl1_nfl2' AND
-      layers_order != 'nfl1_nfl2_nfl3' AND
-      layers_order != 'nfl1_nfl2_nfl3_nfl4'
+  WITH all_layers_numbers AS (
+    SELECT cas_id, 'lyr' || layer::text layer  FROM casfri50.lyr_all
+    UNION ALL
+    SELECT cas_id, 'nfl' || layer::text layer  FROM casfri50.nfl_all
+  ), all_layers_orders AS (
+    SELECT cas_id, string_agg(layer::text, '_' ORDER BY layer) layers_order
+    FROM all_layers_numbers
+    GROUP BY cas_id
+  )
+  SELECT count(*) = 0 passed
+  FROM all_layers_orders
+  WHERE layers_order != 'lyr1' AND
+        layers_order != 'lyr1_lyr2' AND
+        layers_order != 'lyr1_lyr2_lyr3' AND
+        layers_order != 'lyr1_lyr2_lyr3_lyr4' AND
+        layers_order != 'lyr1_lyr2_lyr3_lyr4_lyr5' AND
+        layers_order != 'lyr1_lyr2_lyr3_lyr4_nfl5' AND
+        layers_order != 'lyr1_lyr2_lyr3_lyr4_nfl5_nfl6' AND
+        layers_order != 'lyr1_lyr2_lyr3_lyr4_nfl5_nfl6_nfl7' AND
+        layers_order != 'lyr1_lyr2_lyr3_nfl4' AND
+        layers_order != 'lyr1_lyr2_lyr3_nfl4_nfl5' AND
+        layers_order != 'lyr1_lyr2_lyr3_nfl4_nfl5_nfl6' AND
+        layers_order != 'lyr1_lyr2_lyr3_nfl4_nfl5_nfl6_nfl7' AND
+        layers_order != 'lyr1_lyr2_nfl3' AND
+        layers_order != 'lyr1_lyr2_nfl3_nfl4' AND
+        layers_order != 'lyr1_lyr2_nfl3_nfl4_nfl5' AND
+        layers_order != 'lyr1_lyr2_nfl3_nfl4_nfl5_nfl6' AND
+        layers_order != 'lyr1_nfl2' AND
+        layers_order != 'lyr1_nfl2_nfl3' AND
+        layers_order != 'lyr1_nfl2_nfl3_nfl4' AND
+        layers_order != 'lyr1_nfl2_nfl3_nfl4_nfl5' AND
+        layers_order != 'nfl1' AND
+        layers_order != 'nfl1_nfl2' AND
+        layers_order != 'nfl1_nfl2_nfl3' AND
+        layers_order != 'nfl1_nfl2_nfl3_nfl4'
 ) foo
 -------------------------------------------------------
 UNION ALL
@@ -195,14 +196,14 @@ FROM casfri50.lyr_all
 GROUP BY cas_id
 HAVING NOT (string_agg(layer::text, ''_'' ORDER BY height_upper DESC, layer ASC) = ANY(ARRAY[''1'', ''1_2'', ''1_2_3'', ''1_2_3_4'', ''1_2_3_4_5'']));
 ' list_query
-FROM (
-WITH orderings AS (
-  SELECT DISTINCT string_agg(layer::text, '_' ORDER BY height_upper DESC, layer ASC) lyr_ordering
-  FROM casfri50.lyr_all
-  GROUP BY cas_id
-) 
-SELECT array_agg(lyr_ordering) = ARRAY['1', '1_2', '1_2_3', '1_2_3_4', '1_2_3_4_5'] passed
-FROM orderings
+  FROM (
+  WITH orderings AS (
+    SELECT DISTINCT string_agg(layer::text, '_' ORDER BY height_upper DESC, layer ASC) lyr_ordering
+    FROM casfri50.lyr_all
+    GROUP BY cas_id
+  ) 
+  SELECT array_agg(lyr_ordering) = ARRAY['1', '1_2', '1_2_3', '1_2_3_4', '1_2_3_4_5'] passed
+  FROM orderings
 ) foo
 -------------------------------------------------------
 UNION ALL
@@ -234,23 +235,23 @@ WHERE NOT (CASE WHEN species_1 = ANY(TT_IsMissingOrNotInSetCode()) AND species_1
     ANY(ARRAY['''', ''01'', ''01_02'', ''01_02_03'', ''01_02_03_04'', ''01_02_03_04_05'', ''01_02_03_04_05_06'', ''01_02_03_04_05_06_07'', ''01_02_03_04_05_06_07_08'', ''01_02_03_04_05_06_07_08_09'', ''01_02_03_04_05_06_07_08_09_10''])
     ORDER BY cas_id;
 ' list_query
-FROM (
-WITH species_orderings AS (
-  SELECT DISTINCT 
-    CASE WHEN species_1 = ANY(TT_IsMissingOrNotInSetCode()) AND species_1 != 'NOT_IN_SET' THEN '' ELSE '01' END || 
-    CASE WHEN species_2 = ANY(TT_IsMissingOrNotInSetCode()) AND species_2 != 'NOT_IN_SET' THEN '' ELSE '_02' END || 
-    CASE WHEN species_3 = ANY(TT_IsMissingOrNotInSetCode()) AND species_3 != 'NOT_IN_SET' THEN '' ELSE '_03' END || 
-    CASE WHEN species_4 = ANY(TT_IsMissingOrNotInSetCode()) AND species_4 != 'NOT_IN_SET' THEN '' ELSE '_04' END || 
-    CASE WHEN species_5 = ANY(TT_IsMissingOrNotInSetCode()) AND species_5 != 'NOT_IN_SET' THEN '' ELSE '_05' END || 
-    CASE WHEN species_6 = ANY(TT_IsMissingOrNotInSetCode()) AND species_6 != 'NOT_IN_SET' THEN '' ELSE '_06' END || 
-    CASE WHEN species_7 = ANY(TT_IsMissingOrNotInSetCode()) AND species_7 != 'NOT_IN_SET' THEN '' ELSE '_07' END || 
-    CASE WHEN species_8 = ANY(TT_IsMissingOrNotInSetCode()) AND species_8 != 'NOT_IN_SET' THEN '' ELSE '_08' END || 
-    CASE WHEN species_9 = ANY(TT_IsMissingOrNotInSetCode()) AND species_9 != 'NOT_IN_SET' THEN '' ELSE '_09' END || 
-    CASE WHEN species_10 = ANY(TT_IsMissingOrNotInSetCode()) AND species_10 != 'NOT_IN_SET' THEN '' ELSE '10' END  lyr_ordering
-  FROM casfri50.lyr_all
-) 
-SELECT array_agg(lyr_ordering) = ARRAY['', '01', '01_02', '01_02_03', '01_02_03_04', '01_02_03_04_05', '01_02_03_04_05_06', '01_02_03_04_05_06_07', '01_02_03_04_05_06_07_08', '01_02_03_04_05_06_07_08_09', '01_02_03_04_05_06_07_08_09_10'] passed
-FROM species_orderings
+  FROM (
+  WITH species_orderings AS (
+    SELECT DISTINCT 
+      CASE WHEN species_1 = ANY(TT_IsMissingOrNotInSetCode()) AND species_1 != 'NOT_IN_SET' THEN '' ELSE '01' END || 
+      CASE WHEN species_2 = ANY(TT_IsMissingOrNotInSetCode()) AND species_2 != 'NOT_IN_SET' THEN '' ELSE '_02' END || 
+      CASE WHEN species_3 = ANY(TT_IsMissingOrNotInSetCode()) AND species_3 != 'NOT_IN_SET' THEN '' ELSE '_03' END || 
+      CASE WHEN species_4 = ANY(TT_IsMissingOrNotInSetCode()) AND species_4 != 'NOT_IN_SET' THEN '' ELSE '_04' END || 
+      CASE WHEN species_5 = ANY(TT_IsMissingOrNotInSetCode()) AND species_5 != 'NOT_IN_SET' THEN '' ELSE '_05' END || 
+      CASE WHEN species_6 = ANY(TT_IsMissingOrNotInSetCode()) AND species_6 != 'NOT_IN_SET' THEN '' ELSE '_06' END || 
+      CASE WHEN species_7 = ANY(TT_IsMissingOrNotInSetCode()) AND species_7 != 'NOT_IN_SET' THEN '' ELSE '_07' END || 
+      CASE WHEN species_8 = ANY(TT_IsMissingOrNotInSetCode()) AND species_8 != 'NOT_IN_SET' THEN '' ELSE '_08' END || 
+      CASE WHEN species_9 = ANY(TT_IsMissingOrNotInSetCode()) AND species_9 != 'NOT_IN_SET' THEN '' ELSE '_09' END || 
+      CASE WHEN species_10 = ANY(TT_IsMissingOrNotInSetCode()) AND species_10 != 'NOT_IN_SET' THEN '' ELSE '10' END  lyr_ordering
+    FROM casfri50.lyr_all
+  ) 
+  SELECT array_agg(lyr_ordering) = ARRAY['', '01', '01_02', '01_02_03', '01_02_03_04', '01_02_03_04_05', '01_02_03_04_05_06', '01_02_03_04_05_06_07', '01_02_03_04_05_06_07_08', '01_02_03_04_05_06_07_08_09', '01_02_03_04_05_06_07_08_09_10'] passed
+  FROM species_orderings
 ) foo
 -------------------------------------------------------
 --) foo WHERE NOT passed;
--- workflow/03_flatCASFRI/flatCASFRI_all_layers_same_row_test.sql
+++ workflow/03_flatCASFRI/flatCASFRI_all_layers_same_row_test.sql
@@ -28,10 +28,10 @@ FROM casfri50_flat.cas_flat_all_layers_same_row;
 
 
 -- Check the completeness of STAND_PHOTO_YEAR
-SELECT left(cas_id, 4) inv, stand_photo_year, count(*) nb
+SELECT inventory_id, stand_photo_year, count(*) nb
 FROM casfri50_flat.cas_flat_all_layers_same_row
-GROUP BY inv, stand_photo_year
-ORDER BY inv, stand_photo_year;
+GROUP BY inventory_id, stand_photo_year
+ORDER BY inventory_id, stand_photo_year;
 
 -- All inventories except AB06 and BC08 have holes in STAND_PHOTO_YEAR assignation.
 
