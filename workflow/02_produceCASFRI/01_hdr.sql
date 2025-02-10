0e3c43be3f51eaa4c0c2a06a5d318e1f274aeb5c
DiegoWuGit <153773149+DiegoWuGit@users.noreply.github.com>
2025-02-10 16:52:20 -0500

    #795 - add DS05 in HDR table and regenerate it

--- workflow/02_produceCASFRI/01_hdr.sql
+++ workflow/02_produceCASFRI/01_hdr.sql
@@ -51,7 +51,7 @@ WHERE inventory_id IN ('AB03', 'AB06', 'AB07', 'AB08', 'AB10', 'AB11', 'AB16', '
                        'QC01', 'QC02', 'QC03', 'QC04', 'QC05', 'QC06', 'QC07', 
                        'SK01', 'SK02', 'SK03', 'SK04', 'SK05', 'SK06', 
                        'YT01', 'YT02', 'YT03',
-                       'DS01', 'DS02', 'DS03', 'DS04');
+                       'DS01', 'DS02', 'DS03', 'DS04', 'DS05');
 ------------------------
 SELECT count(*) FROM casfri50.hdr_all; -- 60
 
