a23c9fb95e809a4a7aa7c4ec5163d21ed87845e7
Yassmine Zada <yassminezd7@gmail.com>
2025-03-11 17:04:43 -0400

    #894 - NL02 - Update dev translation script

--- workflow/01_develTranslationTables/NL02.sql
+++ workflow/01_develTranslationTables/NL02.sql
@@ -44,7 +44,7 @@ SELECT * FROM TT_Translate_nl02_nfl_devel('rawfri', 'nl02_l2_to_nl_nli_l2_map');
 -- CAS ATTRIBUTES
 SELECT * FROM translation.nl_nli02_cas;
 DROP TABLE IF EXISTS translation_devel.nl02_nli02_cas_devel;
-CREATE TABLE translation_devel.nl02_nli02_cas_devel AS SELECT * FROM translation.nl_nli02_cas WHERE rule_id::int < 4;
+CREATE TABLE translation_devel.nl02_nli02_cas_devel AS SELECT * FROM translation.nl_nli02_cas WHERE rule_id::int < 10;
 SELECT * FROM translation_devel.nl02_nli02_cas_devel;
 SELECT TT_Prepare('translation_devel', 'nl02_nli02_cas_devel', '_nl02_cas_devel');
 SELECT TT_CreateMappingView('rawfri', 'nl02', 1, 'nl_nli', 2, 200);
@@ -64,13 +64,13 @@ SELECT * FROM TT_Translate_nl02_geo_devel('rawfri', 'nl02_l1_to_nl_nli_l2_map_20
 
 -- ECO ATTRIBUTES
 SELECT * FROM translation.nl_nli02_eco;
-DROP TABLE IF EXISTS translation_devel.nl01_nli01_eco_devel;
-CREATE TABLE translation_devel.nl01_nli01_eco_devel AS
-SELECT * FROM translation.nl_nli01_eco; --WHERE rule_id::int = 1
-SELECT * FROM translation_devel.nl01_nli01_eco_devel;
-SELECT TT_Prepare('translation_devel', 'nl01_nli01_eco_devel', '_nl01_eco_devel');
-SELECT TT_CreateMappingView('rawfri', 'nl01', 'nl_nli', 200);
-SELECT * FROM TT_Translate_nl01_eco_devel('rawfri', 'nl01_l1_to_nl_nli_l1_map_200');
+DROP TABLE IF EXISTS translation_devel.nl02_nli02_eco_devel;
+CREATE TABLE translation_devel.nl02_nli02_eco_devel AS
+SELECT * FROM translation.nl_nli02_eco WHERE rule_id::int = 1;
+SELECT * FROM translation_devel.nl02_nli02_eco_devel;
+SELECT TT_Prepare('translation_devel', 'nl02_nli02_eco_devel', '_nl02_eco_devel');
+SELECT TT_CreateMappingView('rawfri', 'nl02', 'nl_nli', 200);
+SELECT * FROM TT_Translate_nl02_eco_devel('rawfri', 'nl02_l1_to_nl_nli_l2_map_200');
 
 
 -- LYR ATTRIBUTES
