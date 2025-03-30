aef8d7133638c77660b80eb465634a4d80638b88
Yassmine Zada <yassminezd7@gmail.com>
2025-03-29 23:36:31 -0400

    #811 - Update time run constraints

--- workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/03_additionalChecks.sql
+++ workflow/02_produceCASFRI/03_ConstraintsChecksAndIndexes/03_additionalChecks.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------
--- CASFRI - Additional constraints script for CASFRI v5
+-- CASFRI - Additional constraints script for CASFRI v5 --1hr02min50sec
 -- For use with PostgreSQL Table Tranlation Framework v2.0.1 for PostgreSQL 13.x
 -- https://github.com/CASFRI/PostgreSQL-Table-Translation-Framework
 -- https://github.com/CASFRI/CASFRI
