bfa5ed0e0b16f3cdbcc7e3b93fedf4611d229ffc
mtcarto <morgan.thomps@gmail.com>
2025-04-28 13:08:56 -0700

    fix yt helperfunction uninstall calls

--- helperfunctions/helperFunctionsCasfriUninstall.sql
+++ helperfunctions/helperFunctionsCasfriUninstall.sql
@@ -70,7 +70,7 @@ DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_mapText(text, text, text, text, 
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_notNull(text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_hasCountOfLayers(text, text, text, text);
 DROP FUNCTION IF EXISTS TT_yvi03_nat_non_veg_validation(text,text,text, text);
-DROP FUNCTION IF EXISTS TT_yvi03_nfl_soil_moisture_validation(text,text,text,text);
+DROP FUNCTION IF EXISTS TT_yvi03_nfl_soil_moisture_validation(text,text,text,text,text);
 DROP FUNCTION IF EXISTS TT_fim_species_count_validate(text, text);
 DROP FUNCTION IF EXISTS TT_pe_pei01_dist_type_length_validation(text, text, text);
 DROP FUNCTION IF EXISTS TT_mb_mb03_disturbance_hasCountOfNotNull(text, text);
@@ -170,9 +170,9 @@ DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_copyText(text, text, text, text,
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_copyInt(text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_matchList(text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_yvi03_nat_non_veg_translation(text, text, text, text);
-DROP FUNCTION IF EXISTS TT_yvi03_non_for_veg_translation(text, text, text);
 DROP FUNCTION IF EXISTS TT_pe_pei01_map_dist_year(text);
 DROP FUNCTION IF EXISTS TT_vri01_src_inv_area_translation(text, text);
+DROP FUNCTION IF EXISTS TT_yvi03_non_for_veg_translation(text, text);
 DROP FUNCTION IF EXISTS TT_nt_lyr_layer_translation(text, text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_mb_mb03_map_disturbance(text, text, text);
 -------------------------------------------------------------------------------
@@ -194,7 +194,7 @@ DROP FUNCTION IF EXISTS TT_mb_fri01_wetland_code(text, text);
 DROP FUNCTION IF EXISTS TT_pc02_wetland_code(text, text, text);
 DROP FUNCTION IF EXISTS TT_qc_prg5_species_code_to_reordered_array(text);
 DROP FUNCTION IF EXISTS TT_yt_wetland_code(text, text, text, text, text, text, text);
-DROP FUNCTION IF EXISTS TT_yt04_wetland_code(text, text, text, text, text, text, text);
+DROP FUNCTION IF EXISTS TT_yt04_wetland_string_code(text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_qc_prg5_species(text, text);
 DROP FUNCTION IF EXISTS TT_qc_prg4_species(text, text);
 -------------------------------------------------------------------------------
