f2eb0fbe7d5990a80683bf3d014a89fb968488b0
pedrogit <pierre.racine@sbf.ulaval.ca>
2025-10-06 11:23:56 -0400

    #814 Closes #824 Make dump of test translated tables run in parallel

--- translation/test/dump_test_tables.sh
+++ translation/test/dump_test_tables.sh
@@ -10,7 +10,6 @@
 source ../../conversion/sh/common.sh
 
 pgversion=${pgversion:=13}
-#pgversion=${pgversion:=12} 
 
 # ########################################## Process ######################################
 
@@ -18,12 +17,12 @@ pgversion=${pgversion:=13}
 
 rm ./data$pgversion/*.csv
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\cas_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED -progress "casfri50_test.cas_all_new_ordered"
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\cas_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED "casfri50_test.cas_all_new_ordered" &
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\dst_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED -progress "casfri50_test.dst_all_new_ordered" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\dst_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED "casfri50_test.dst_all_new_ordered" &
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\eco_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED -progress "casfri50_test.eco_all_new_ordered" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\eco_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED "casfri50_test.eco_all_new_ordered" &
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\lyr_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED -progress "casfri50_test.lyr_all_new_ordered" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\nfl_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED "casfri50_test.nfl_all_new_ordered" &
 
-"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\nfl_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED -progress "casfri50_test.nfl_all_new_ordered" 
+"$gdalFolder/ogr2ogr" -f "CSV" ".\data$pgversion\lyr_all_test.csv" "$pg_connection_string" -lco STRING_QUOTING=IF_NEEDED -progress "casfri50_test.lyr_all_new_ordered"
