bbb62a2b1522e67f381d3a22a000b3b1892a3ee2
pedrogit <pierre.racine@sbf.ulaval.ca>
2025-10-06 09:22:28 -0400

    #814 Reordered the list of FRI to process for easier comparison. Always keep the biggest last in its group.

--- configSample.sh
+++ configSample.sh
@@ -30,38 +30,28 @@ targetTranslationFileSchema=translation
 # Extra, non inventory tables to load (not translate)
 photoYearList=(ab_photoyear nl_photoyear nl02_photoyear)
 
-# First series of inventories having more than 2000000 rows. 
+# Inventories loading is ordered by province and size.
 # IMPORTANT: Longest one should be last because the next series will start only after the last.
-invList1=(BC04 BC11 QC05 QC01)
-invList1CFS=(BC11 BC16 QC08 QC09 QC10)
+invList1=(BC04 BC08 BC10 BC12 BC13 BC11)
+invList1CFS=(BC08 BC10 BC12 BC13 BC14 BC15 BC16 BC18 BC11)
 
-# Second series of inventories having more than 2000000 rows.
-# IMPORTANT: Longest one should be last because the next series will start only after the last.
-invList2=(BC08 QC06 BC12 BC10)
-invList2CFS=(BC08 QC04 QC05 BC12 BC10)
+invList2=(QC01 QC02 QC03 QC04 QC06 QC07 QC05)
+invList2CFS=(QC03 QC04 QC08 QC09 QC10 QC05)
 
-# Third series of inventories having more than 2000000 rows.
-# IMPORTANT: Longest one should be last because the next series will start only after the last.
-invList3=(QC04 QC02 ON01 ON02 NL02)
-invList3CFS=(ON02 BC13 BC14 BC15)
+invList3=(NL01 NL02 ON02 ON01)
+invList3CFS=(NL02 ON02)
 
-# Inventories having between 800000 and 2000000 rows.
-# IMPORTANT: Longest one should be last because the next series will start only after the last.
-invList4=(AB32 NB01 NB02 NS01 NS02 NS03 SK01 NL01 MB05)
-invList4CFS=(NB02 NB03 NS03 NS04)
+invList4=(PE01 PE02 MB01 MB02 MB04 MB06 MB07 MB08 MB05)
+invList4CFS=(PE01 PE02 MB03 MB06 MB07 MB10 MB11 MB12 MB13)
 
-# Inventories having between 240000 and 800000 rows.
-# IMPORTANT: Longest one should be last because the next series will lauch after the last.
-invList5=(AB21 AB25 AB29 AB31 SK04 NT01 NT03 QC03 YT01 SK05)
-invList5CFS=(AB29 NT03 NT04 QC03)
+invList5=(PE03 PE04 NB01 NB02 SK02 SK03 SK04 SK05 SK06 SK01)
+invList5CFS=(PE03 PE04 NB02 NB03)
 
-# Inventories having less than 200000 rows
-invList6=(MB06 MB07 MB08 PC01 PC02 PE01 PE02 PE03 PE04 QC07 SK02 SK03 SK06 YT02 YT03)
-invList6CFS=(MB06 MB07 MB10 MB11 MB12 MB13 PE01 PE02 PE03 PE04 YT03 YT04)
+invList6=(PC01 PC02 NT01 NT03 YT01 YT02 YT03 NS02 NS03 NS01)
+invList6CFS=(NT03 NT04 YT03 YT04 NS03 NS04)
 
-# Inventories having less than 200000 rows
-invList7=(AB03 AB06 AB07 AB08 AB10 AB11 AB16 AB24 AB27 AB30 AB34 MB01 MB02 MB04)
-invList7CFS=(AB30 MB03)
+invList7=(AB03 AB06 AB07 AB08 AB10 AB11 AB16 AB21 AB24 AB25 AB27 AB29 AB30 AB31 AB34 AB32)
+invList7CFS=(AB29 AB30)
 
 # Additionnal disturbances (raster and vector)
 invList8=(DS01 DS02 DS03 DS04 DS05)
