e83825c58c40c03d5ffabfdb3f90ad824020e42c
pedrogit <pierre.racine@sbf.ulaval.ca>
2025-10-06 13:42:20 -0400

    #814 Added an option to prevent cmd windows to close for easier debugging

--- translation/test/testTranslation.sh
+++ translation/test/testTranslation.sh
@@ -2,12 +2,15 @@
 
 source ../../config.sh
 
-"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_cas.sql" &
+# uncomment this to prevent the windows from closing at the end of the script for debug purpose
+#dontclose="read -p 'Press enter close the command window...';"
 
-"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_dst.sql" &
+"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_cas.sql;$dontclose" &
 
-"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_eco.sql" &
+"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_dst.sql;$dontclose" &
 
-"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_lyr.sql" &
+"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_eco.sql;$dontclose" &
 
-"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_nfl.sql" &
+"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_lyr.sql;$dontclose" &
+
+"/c/program files/git/git-bash.exe" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./test_nfl.sql;$dontclose" &
