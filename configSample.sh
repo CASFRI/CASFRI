93da9f3efccd0061f77c8acc19f652c57688e57f
pedrogit <pierre.racine@sbf.ulaval.ca>
2025-12-12 15:47:18 -0500

    #814 Clearer configuration of the list of inventories to translate

--- configSample.sh
+++ configSample.sh
@@ -24,41 +24,51 @@ targetFRISchema=rawfri
 targetTranslationFileSchema=translation
 
 #################################################################
-# List of source tables to load and translate
+# List of source tables to load and translate with load_all.sh
 #################################################################
 
 # Extra, non inventory tables to load (not translate)
 photoYearList=(ab_photoyear nl_photoyear nl02_photoyear)
 
-# Inventories loading is ordered by province and size.
-# IMPORTANT: Longest one should be last because the next series will start only after the last.
-invList1=(BC04 BC08 BC10 BC12 BC13 BC11)
-invList1CFS=(BC08 BC10 BC12 BC13 BC14 BC15 BC16 BC18 BC11)
+# Inventories are ordered by province and size. All inventories listed 
+# in the same invListX variable are loaded in parallel.
 
-invList2=(QC01 QC02 QC03 QC04 QC06 QC07 QC08 QC09 QC10 QC05)
-invList2CFS=(QC03 QC04 QC08 QC09 QC10 QC05)
+# IMPORTANT: Biggest inventories (having the biggest number of rows) should be 
+# listed last in their own variable (e.g. invList2) because the next series in 
+# the next variable (e.g. invList3) will start only after the last inventory is 
+# done loading. This prevents subsequent series from starting before previous 
+# ones are finished.
 
-invList3=(NL01 NL02 ON02 ON01)
-invList3CFS=(NL02 ON02)
+invList1=(BC04 BC08 BC10 BC12 BC13 BC14 BC15 BC16 BC17 BC18 BC11)
 
-invList4=(PE01 PE02 MB01 MB02 MB04 MB06 MB07 MB08 MB05)
-invList4CFS=(PE01 PE02 MB03 MB06 MB07 MB10 MB11 MB12 MB13)
+invList2=(QC03 QC04 QC08 QC09 QC10 QC05)
 
-invList5=(PE03 PE04 NB01 NB02 SK02 SK03 SK04 SK05 SK06 SK07 SK01)
-invList5CFS=(PE03 PE04 NB02 NB03 SK07)
+invList3=(NL02 PE01 PE02 PE03 PE04 MB06 MB07 MB08 ON02)
 
-invList6=(PC01 PC02 NT01 NT03 YT01 YT02 YT03 NS02 NS03 NS04 NS01)
-invList6CFS=(NT03 NT04 YT03 YT04 NS03 NS04)
+invList4=(NB02 NB03 NB04 NB05 NB06 NB07 NB08 SK07)
 
-invList7=(AB03 AB06 AB07 AB08 AB10 AB11 AB16 AB21 AB24 AB25 AB27 AB29 AB30 AB31 AB34 AB32)
-invList7CFS=(AB29 AB30)
+invList5=(AB29 AB30 NT03 NT04 YT03 YT04 NS01 NS03 NS04)
 
-# Additionnal disturbances (raster and vector)
-invList8=(DS01 DS02 DS03 DS04 DS05)
+# CFS only inventory lists
+invListCFSOnly=(MB03 MB10 MB11 MB12 MB13)
+
+# ULaval only inventory lists
+invListULavalOnly1=(BC04 QC07 NL01 ON01 MB01 MB02 MB04 MB05 NB01 QC02 QC06 QC01)
+invListULavalOnly2=(NT01 YT01 YT02 NS01 NS02 PC01 PC02 SK02 SK03 SK04 SK05 SK06 SK01)
+invListULavalOnly3=(AB03 AB06 AB07 AB08 AB10 AB11 AB16 AB21 AB24 AB25 AB27 AB31 AB34 AB32)
+
+# Additionnal disturbances (raster and vector, ULaval only)
+invListULavalOnly4=(DS01 DS02 DS03 DS04 DS05)
 
 # Merge all inventory lists into a list of list
-fullList=(photoYearList invList1 invList2 invList3 invList4 invList5 invList6 invList7 invList8)
-#fullList=(invList1CFS invList2CFS invList3CFS invList4CFS invList5 iCFSt6 invList7CFS)
+fullList=(photoYearList invList1 invList2 invList3 invList4 invList5 invListCFSOnly)
+#fullList=(photoYearList invList1 invList2 invList3 invList4 invList5 invListULavalOnly1 invListULavalOnly2 invListULavalOnly3 invListULavalOnly4)
+
+# This is for when some inventories failed during the first attempt at loading everything
+# Just drop the failed tables from the database, list them here and load_all.sh.
+extraList1=()
+extraList2=()
+#fullList=(extraList1 extraList2)
 
 #################################################################
 # GDAL executable folder
