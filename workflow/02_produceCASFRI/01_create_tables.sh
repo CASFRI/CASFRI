a2103407effd2c66d816740ab991a02b2e90cc6e
pedrogit <pierre.racine@sbf.ulaval.ca>
2026-02-10 11:53:52 -0500

    #819 Delete obsolete translation scripts and rename main translation scripts

--- workflow/02_produceCASFRI/01_create_tables.sh
+++ workflow/02_produceCASFRI/01_create_tables.sh
@@ -4,6 +4,6 @@ source ../../config.sh
 
 "$bashCmd" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f 01_hdr.sql" &
 
-"$bashCmd" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f ./02_perInventory/01_createTables.sql" &
+"$bashCmd" -c "$pgFolder/bin/psql -p $pgport -U $pguser -w -d $pgdbname -P pager=off -f 01_createTables.sql" &
 
 wait
